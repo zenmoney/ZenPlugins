83870fe2aba7c10086906c5642c809f4e2b8d533
Metallium <dev.nkh@gmail.com>
2017-02-15 22:04:36 +0300

    [belswissbank] code cleanup

--- plugins/belswissbank/main.js
+++ plugins/belswissbank/main.js
@@ -52,6 +52,10 @@ var main = (function (_, utils, BSB, ZenMoney, errors) {
         }
     }
 
+    function calculateAccountId(card) {
+        return card.cardId.toString();
+    }
+
     function mergeTransferTransactions(transactionsForAccounts) {
         var transactionReplacements = _.chain(transactionsForAccounts)
             .pluck('transfers')
@@ -85,129 +89,121 @@ var main = (function (_, utils, BSB, ZenMoney, errors) {
         });
     }
 
-    return function main() {
-        ZenMoney.trace('alive @' + Date.now());
+    function processCards(cards) {
+        return cards.filter(function (card) {
+            return !isAccountSkipped(calculateAccountId(card));
+        }).map(processCard);
+    }
 
-        login();
+    function processCard(card) {
+        var lastFourDigits = card.maskedCardNumber.slice(-4);
+        var accountCurrency = BSB.getIsoCurrency(card.currency);
+        var account = {
+            id: calculateAccountId(card),
+            title: card.name,
+            type: 'ccard',
+            syncID: [lastFourDigits],
+            instrument: accountCurrency,
+            balance: card.amount
+        };
 
-        var cards = BSB.getCards();
+        var lastSeenBsbTransaction = ZenMoney.getData('lastSeenBsbTransactionFor' + account.id, {
+            cardTransactionId: 0,
+            accountRest: 0,
+            transactionDate: BSB.bankBirthday.valueOf()
+        });
 
-        var transactionsForAccounts = cards.map(function (card) {
-            var lastFourDigits = card.maskedCardNumber.slice(-4);
-            var accountCurrency = BSB.getIsoCurrency(card.currency);
-            var account = {
-                id: card.cardId.toString(),
-                title: card.name,
-                type: 'ccard',
-                syncID: [lastFourDigits],
-                instrument: accountCurrency,
-                balance: card.amount
-            };
+        var syncFrom = new Date(lastSeenBsbTransaction.transactionDate);
+        var syncTo = new Date();
 
-            if (isAccountSkipped(account.id)) {
-                ZenMoney.trace('skipped account ' + account.id);
-                return;
-            }
+        ZenMoney.trace(
+            'fetching transactions for ' + utils.toReadableJson({
+                accountId: account.id,
+                from: formatDetailsDateTime(syncFrom),
+                to: formatDetailsDateTime(syncTo)
+            }));
 
-            var lastSeenBsbTransaction = ZenMoney.getData('lastSeenBsbTransactionFor' + account.id, {
-                cardTransactionId: 0,
-                accountRest: 0,
-                transactionDate: BSB.bankBirthday.valueOf()
-            });
+        var bsbTransactions = _.chain(BSB.getTransactions(card.cardId, syncFrom, syncTo))
+            .filter(function (transaction) {
+                return transaction.transactionType !== 'Otkaz' &&
+                    transaction.transactionAmount > 0 &&
+                    transaction.cardTransactionId > lastSeenBsbTransaction.cardTransactionId;
+            })
+            .sortBy(_.property('cardTransactionId'))
+            .value();
+        var newLastSeenBsbTransaction = _.last(bsbTransactions);
+        return bsbTransactions
+            .reduce(function (result, transaction, index, transactions) {
+                var factor = BSB.getFactor(transaction);
+                var transactionDelta = factor * transaction.transactionAmount;
 
-            var syncFrom = new Date(lastSeenBsbTransaction.transactionDate);
-            var syncTo = new Date();
+                var transactionCurrency = transaction.transactionCurrency;
+                var isCurrencyConversion = transactionCurrency !== accountCurrency;
 
-            ZenMoney.trace(
-                'fetching transactions for ' + utils.toReadableJson({
-                    accountId: account.id,
-                    from: formatDetailsDateTime(syncFrom),
-                    to: formatDetailsDateTime(syncTo)
-                }));
-
-            var bsbTransactions = _.chain(BSB.getTransactions(card.cardId, syncFrom, syncTo))
-                .filter(function (transaction) {
-                    return transaction.transactionType !== 'Otkaz' &&
-                        transaction.transactionAmount > 0 &&
-                        transaction.cardTransactionId > lastSeenBsbTransaction.cardTransactionId;
-                })
-                .sortBy(_.property('cardTransactionId'))
-                .value();
-            var memo = bsbTransactions
-                .reduce(function (memo, transaction, index, transactions) {
-                    var factor = BSB.getFactor(transaction);
-                    var transactionDelta = factor * transaction.transactionAmount;
-
-                    var transactionCurrency = transaction.transactionCurrency;
-                    var isCurrencyConversion = transactionCurrency !== accountCurrency;
-
-                    var previousTransaction = index === 0 ? lastSeenBsbTransaction : transactions[index - 1];
-                    if (transaction.accountRest === null) {
-                        if (isCurrencyConversion) {
-                            throw errors.fatal('cannot determine missing accountRest for currency conversion: ' + utils.toReadableJson(transaction));
-                        }
-                        transaction.accountRest = previousTransaction.accountRest + transactionDelta;
-                    }
-                    var accountDelta = isCurrencyConversion ?
-                        transaction.accountRest - previousTransaction.accountRest :
-                        transactionDelta;
-
-                    var zenMoneyTransaction = {
-                        id: transaction.cardTransactionId.toString(),
-                        date: transaction.transactionDate,
-                        payee: _.compact([transaction.countryCode, transaction.city, transaction.transactionDetails]).join('/'),
-                        comment: formatDetailsDateTime(new Date(transaction.transactionDate))
-                    };
-                    memo.transactions.push(zenMoneyTransaction);
-
-                    var absAccountDelta = Math.abs(accountDelta);
-                    var absTransactionDelta = Math.abs(transactionDelta);
-
+                var previousTransaction = index === 0 ? lastSeenBsbTransaction : transactions[index - 1];
+                if (transaction.accountRest === null) {
                     if (isCurrencyConversion) {
-                        zenMoneyTransaction.comment += '\n' + absTransactionDelta.toFixed(2) + ' ' + transactionCurrency;
+                        throw errors.fatal('cannot determine missing accountRest for currency conversion: ' + utils.toReadableJson(transaction));
                     }
-                    var isIncome = factor >= 0;
-                    if (transaction.transactionType === 'Bankomat') { // cash
-                        if (isIncome) {
-                            throw errors.fatal('income via Bankomat is not supported: ' + utils.toReadableJson(transaction));
-                        }
-                        zenMoneyTransaction.income = absTransactionDelta;
-                        zenMoneyTransaction.outcome = absAccountDelta;
-                        zenMoneyTransaction.incomeAccount = 'cash#' + transactionCurrency;
-                        zenMoneyTransaction.outcomeAccount = account.id;
-                    } else if (isIncome) { // income
-                        zenMoneyTransaction.income = absAccountDelta;
-                        zenMoneyTransaction.outcome = 0;
-                        zenMoneyTransaction.incomeAccount = account.id;
-                        zenMoneyTransaction.outcomeAccount = account.id;
-                        if (isCurrencyConversion) {
-                            zenMoneyTransaction.opIncome = absTransactionDelta;
-                            zenMoneyTransaction.opIncomeInstrument = transactionCurrency;
-                        }
-                    } else { // outcome
-                        zenMoneyTransaction.income = 0;
-                        zenMoneyTransaction.outcome = absAccountDelta;
-                        zenMoneyTransaction.incomeAccount = account.id;
-                        zenMoneyTransaction.outcomeAccount = account.id;
-                        if (isCurrencyConversion) {
-                            zenMoneyTransaction.opOutcome = absTransactionDelta;
-                            zenMoneyTransaction.opOutcomeInstrument = transactionCurrency;
-                        }
-                    }
-                    if (transaction.transactionDetails === 'PERSON TO PERSON I-B BSB') {
-                        memo.transfers.push({
-                            transferId: [transaction.transactionDate, absTransactionDelta, transaction.transactionCurrency].join('|'),
-                            transaction: zenMoneyTransaction
-                        });
-                    }
-                    return memo;
-                }, {transactions: [], transfers: []});
+                    transaction.accountRest = previousTransaction.accountRest + transactionDelta;
+                }
+                var accountDelta = isCurrencyConversion ?
+                    transaction.accountRest - previousTransaction.accountRest :
+                    transactionDelta;
 
-            var newLastSeenBsbTransaction = _.last(bsbTransactions);
-            return {
+                var zenMoneyTransaction = {
+                    id: transaction.cardTransactionId.toString(),
+                    date: transaction.transactionDate,
+                    payee: _.compact([transaction.countryCode, transaction.city, transaction.transactionDetails]).join('/'),
+                    comment: formatDetailsDateTime(new Date(transaction.transactionDate))
+                };
+                result.transactions.push(zenMoneyTransaction);
+
+                var absAccountDelta = Math.abs(accountDelta);
+                var absTransactionDelta = Math.abs(transactionDelta);
+
+                if (isCurrencyConversion) {
+                    zenMoneyTransaction.comment += '\n' + absTransactionDelta.toFixed(2) + ' ' + transactionCurrency;
+                }
+                var isIncome = factor >= 0;
+                if (transaction.transactionType === 'Bankomat') { // cash
+                    if (isIncome) {
+                        throw errors.fatal('income via Bankomat is not supported: ' + utils.toReadableJson(transaction));
+                    }
+                    zenMoneyTransaction.income = absTransactionDelta;
+                    zenMoneyTransaction.outcome = absAccountDelta;
+                    zenMoneyTransaction.incomeAccount = 'cash#' + transactionCurrency;
+                    zenMoneyTransaction.outcomeAccount = account.id;
+                } else if (isIncome) { // income
+                    zenMoneyTransaction.income = absAccountDelta;
+                    zenMoneyTransaction.outcome = 0;
+                    zenMoneyTransaction.incomeAccount = account.id;
+                    zenMoneyTransaction.outcomeAccount = account.id;
+                    if (isCurrencyConversion) {
+                        zenMoneyTransaction.opIncome = absTransactionDelta;
+                        zenMoneyTransaction.opIncomeInstrument = transactionCurrency;
+                    }
+                } else { // outcome
+                    zenMoneyTransaction.income = 0;
+                    zenMoneyTransaction.outcome = absAccountDelta;
+                    zenMoneyTransaction.incomeAccount = account.id;
+                    zenMoneyTransaction.outcomeAccount = account.id;
+                    if (isCurrencyConversion) {
+                        zenMoneyTransaction.opOutcome = absTransactionDelta;
+                        zenMoneyTransaction.opOutcomeInstrument = transactionCurrency;
+                    }
+                }
+                if (transaction.transactionDetails === 'PERSON TO PERSON I-B BSB') {
+                    result.transfers.push({
+                        transferId: [transaction.transactionDate, absTransactionDelta, transaction.transactionCurrency].join('|'),
+                        transaction: zenMoneyTransaction
+                    });
+                }
+                return result;
+            }, {
                 account: account,
-                transactions: memo.transactions,
-                transfers: memo.transfers,
+                transactions: [],
+                transfers: [],
                 onBeforeSave: newLastSeenBsbTransaction
                     ? function () {
                         var data = _.pick(newLastSeenBsbTransaction, 'accountRest', 'transactionDate', 'cardTransactionId');
@@ -215,12 +211,15 @@ var main = (function (_, utils, BSB, ZenMoney, errors) {
                         ZenMoney.saveData();
                     }
                     : _.noop
-            }
-        });
+            });
+    }
 
-        transactionsForAccounts = mergeTransferTransactions(transactionsForAccounts);
+    return function main() {
+        ZenMoney.trace('alive @' + Date.now());
 
-        transactionsForAccounts.forEach(function (accountTransactions) {
+        login();
+
+        mergeTransferTransactions(processCards(BSB.getCards())).forEach(function (accountTransactions) {
             var account = accountTransactions.account;
             var transactions = accountTransactions.transactions;
             var onBeforeSave = accountTransactions.onBeforeSave;
