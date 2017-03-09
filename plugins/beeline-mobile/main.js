f3d273c886a7c97bb371ae3c0f876803ee48fd29
Dmitry Cheremushkin <Dmitry.Cheremushkin@gmail.com>
2017-03-10 00:29:30 +0300

    [beeline-mobile] Мелкое улучшение для обработки ошибок при получении списка аккаунтов

--- plugins/beeline-mobile/main.js
+++ plugins/beeline-mobile/main.js
@@ -291,7 +291,7 @@ function BeelineBank(ZenMoney) {
      */
     function syncAccount() {
         var profile = doRequest(REQUESTS.GET_ACCOUNT, null);
-        if (profile.hasOwnProperty("response") || !profile.hasOwnProperty("auxInfo")) {
+        if (profile.hasOwnProperty("error") || !profile.hasOwnProperty("auxInfo")) {
             ZenMoney.trace('Ошибка при получении списка аккаунтов: ' + JSON.stringify(profile));
             throw new ZenMoney.Error('Не удалось загрузить список аккаунтов');
         }
