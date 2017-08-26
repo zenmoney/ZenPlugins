421056e36e4fe4120d398f802a252d7327b09aef
Metallium <dev.nkh@gmail.com>
2017-08-26 14:36:59 +0300

    rm plugin id prefix from console (noisy), add to document title (still reasonable when developing multiple plugins)

--- src/windowLoader.js
+++ src/windowLoader.js
@@ -6,13 +6,13 @@ const fetchData = () => fetchJson("/zen/data");
 
 window.onload = function() {
     Promise.all([fetchPreferences(), fetchManifest(), fetchData()])
-        .then(([preferences, manifest, data]) => {
-            const worker = new Worker("/workerLoader.js");
-            worker.postMessage({manifest, preferences, data});
-            worker.onmessage = function(e) {
-                if (e && (e === "close" || e.data === "close")) {
-                    worker.terminate();
-                }
-            };
-        });
+      .then(([preferences, manifest, data]) => {
+        document.title = `[${manifest.id}] ${document.title}`;const worker = new Worker("/workerLoader.js");
+        worker.postMessage({manifest, preferences, data});
+        worker.onmessage = function(e) {
+            if (e && (e === "close" || e.data === "close")) {
+                worker.terminate();
+            }
+        };
+    });
 };
