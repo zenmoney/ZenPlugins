10d16d95cd9747af88ee96276f102b864c2dc1bb
Metallium <dev.nkh@gmail.com>
2017-09-10 17:13:01 +0300

    [bootloader] implementation

---
+++ src/plugins/bootloader/index.js
@@ -0,0 +1,12 @@
+export function main() {
+    const {scriptUrl, preferencesJson} = ZenMoney.getPreferences();
+    const preferences = JSON.parse(preferencesJson);
+    ZenMoney.getPreferences = () => preferences;
+    const body = ZenMoney.requestGet(scriptUrl);
+    const status = ZenMoney.getLastStatusCode();
+    console.assert(status === 200, "non-success status received for url", {status, scriptUrl, body});
+    // eslint-disable-next-line no-eval
+    eval(body);
+    console.assert(global.main !== main, "loaded script should override main function");
+    global.main();
+}
