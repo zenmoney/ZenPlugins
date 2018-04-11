cb9f39c6ea7360b1831a6e709b426600ec8a6625
Nikita <dev.nkh@gmail.com>
2018-04-11 17:46:54 +0300

    rm unused stuff from ZPAPIError

--- src/ZPAPIError.js
+++ src/ZPAPIError.js
@@ -1,63 +1,10 @@
-function ZPAPIError(message, logIsNotImportant, forcePluginReinstall) {
-    message = message || "";
-    let self;
-    if (this instanceof ZPAPIError) {
-        self = this;
-        self.toString = function() {
-            return (this.fatal ? "F" : "_") +
-                (this.allowRetry ? "R" : "_") + " " + this.message;
-        };
-        if (message instanceof ZPAPIError) {
-            self.message = message.message;
-            self.allowRetry = message.allowRetry;
-            self.fatal = message.fatal;
-            self.stack = message.stack;
-        } else {
-            self.message = message.message !== undefined ? message.message : message.toString();
-            if (message.stack !== undefined ||
-                message.stacktrace !== undefined) {
-                self.stack = message.stack || message.stacktrace;
-            } else if (!self.stack) {
-                const error = new Error();
-                self.stack = error.stack || error.stacktrace;
-            }
-            if (typeof self.stack === "string") {
-                self.stack = self.stack.split("\n");
-            }
-            if (self.stack && self.stack.length > 0) {
-                self.stack = self.stack.map(function(item) {
-                    return item.trim();
-                }).filter(function(item) {
-                    return item.length > 0 &&
-                        item.indexOf("handleException") < 0 &&
-                        item.indexOf("ZPAPI") < 0 &&
-                        item !== "Error" &&
-                        item !== "global code" &&
-                        item !== "[native code]";
-                }).join("\n");
-            } else {
-                self.stack = null;
-            }
+export class ZPAPIError extends Error {
+    constructor(message, logIsNotImportant, forcePluginReinstall) {
+        super(message);
+        if (typeof message !== "string") {
+            throw new Error("message must be string");
         }
-        if (message.arguments) {
-            self.arguments = message.arguments;
-        }
-    } else {
-        if (message instanceof ZPAPIError) {
-            self = message;
-        } else {
-            self = new ZPAPIError(message);
-        }
+        this.fatal = Boolean(forcePluginReinstall);
+        this.allowRetry = Boolean(logIsNotImportant);
     }
-    if (self.fatal === undefined || forcePluginReinstall !== undefined) {
-        self.fatal = !!forcePluginReinstall;
-    }
-    if (self.allowRetry === undefined || logIsNotImportant !== undefined) {
-        self.allowRetry = !!logIsNotImportant;
-    }
-    return self;
 }
-
-ZPAPIError.prototype = Error.prototype;
-
-export {ZPAPIError};
