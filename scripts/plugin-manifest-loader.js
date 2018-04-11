d5601791558e0fbb7229742c216a48386eba3581
Nikita <dev.nkh@gmail.com>
2018-04-11 17:23:59 +0300

    make modular plugins contract stricter: disallow changing manifest files section; check there are no redundant exports from index

--- scripts/plugin-manifest-loader.js
+++ scripts/plugin-manifest-loader.js
@@ -1,28 +1,46 @@
 const {convertManifestXmlToJs} = require("./utils");
+const _ = require("lodash");
 
-module.exports = function(xml) {
-    this.cacheable && this.cacheable();
-    const manifest = convertManifestXmlToJs(xml);
-    if (manifest.modular === "true") {
-        return `
-require("polyfills");
+const expectedModularManifestFilesSection = `<files>
+    <js>index.js</js>
+    <preferences>preferences.xml</preferences>
+</files>`;
+
+const repositoryUri = "https://github.com/zenmoney/ZenPlugins/blob/master";
+function generateModularLoader({files, preferences}) {
+    if (!_.isEqual(files, ["index.js"]) || preferences !== "preferences.xml") {
+        throw new Error("modular ZenMoneyManifest.xml files section must be exactly equal to:\n" + expectedModularManifestFilesSection);
+    }
+    return `require("polyfills");
 var result = require("./index");
-if (result.scrape && result.main || !result.scrape && !result.main) {
-    throw new Error("Either scrape or main function should be exported from plugin index file");
+
+var exportsThatMakeNoDifference = Object.keys(result).filter((key) => key !== "main" && key !== "scrape");
+if (exportsThatMakeNoDifference.length > 0) {
+    console.error(exportsThatMakeNoDifference + " members exported by index.js are not handled by ZenMoney, thus looking like a developer mistake (exports are useless and make no effect). It is recommended to stop exporting them.  See: ${repositoryUri}");
 }
+
+if (result.scrape && result.main || !result.scrape && !result.main) {
+    throw new Error("Either scrape() or main() should be exported from index.js (not both). See: ${repositoryUri}");
+}
+
 if (result.scrape) {
     var adaptScrapeToMain = require("adapters").adaptScrapeToMain;
     var main = adaptScrapeToMain(result.scrape);
     result = Object.assign({}, result, {main: main});
 }
-for (var key in result) {
-    if (result.hasOwnProperty(key)) {
-        global[key] = result[key];
-    }
+
+Object.assign(global, result);
+
+module.exports = result;
+`;
 }
-module.exports = result;`;
-    } else {
-        return manifest.files.map(f => `require(${JSON.stringify("!!script-loader!./" + f)});`)
+module.exports = function(xml) {
+    this.cacheable && this.cacheable();
+    const pluginManifest = convertManifestXmlToJs(xml);
+    if (pluginManifest.modular !== "true") {
+        return pluginManifest.files
+            .map((file) => `require(${JSON.stringify("!!script-loader!./" + file)});`)
             .concat(`module.exports = {main: global.main};`).join("\n");
     }
+    return generateModularLoader(pluginManifest);
 };
