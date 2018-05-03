91c2291944541b029ff4cdc7ce3d60fb65de8cda
Nikita <dev.nkh@gmail.com>
2018-05-03 22:07:32 +0300

    temporarily override console.assert behavior to throw in node 10+ (we rely on that in some cases)

--- scripts/setupJestTestFramework.js
+++ scripts/setupJestTestFramework.js
@@ -1,4 +1,12 @@
+import {formatWithCustomInspectParams} from "../src/consoleAdapter";
+
 console.debug = process.env.DEBUG ? function() {
     console.log("[console.debug]", ...arguments);
 } : function() {
+};
+
+console.assert = function(condition, ...args) {
+    if (!condition) {
+        throw new Error(formatWithCustomInspectParams(...args));
+    }
 };
