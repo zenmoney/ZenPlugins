e77b75eb6ffe6f399e6ffac8fb9306818da2efab
skvav <konstantin.sychkin@gmail.com>
2018-05-17 21:20:08 +0300

    jscs updated to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,46 @@ module.exports = {
         "TemporaryError": false,
         "InvalidPreferencesError": false,
     },
+    "rules": {
+        "comma-dangle": [
+            2,
+            "always-multiline",
+        ],
+        "no-implicit-coercion": [
+            2,
+            {
+                "boolean": true,
+                "string": true,
+                "number": true,
+            },
+        ],
+        "brace-style": [
+            2,
+            "1tbs",
+            {
+                "allowSingleLine": true
+            },
+        ],
+        "no-with": 2,
+        "no-multiple-empty-lines": 2,
+        "no-multi-spaces": 2,
+        "one-var": [
+            2,
+            "never",
+        ],
+        "indent": [
+            2,
+            4,
+            {
+                "SwitchCase": 1,
+            },
+        ],
+        "quotes": [
+            2,
+            "double",
+            {
+                "allowTemplateLiterals": true,
+            },
+        ],
+    },
 };
