17dacc4c27646ab79f0539ae8cb3bdd5efbd3eea
Nikita <dev.nkh@gmail.com>
2018-05-18 10:21:45 +0300

    [alfabank] normalize phone number

--- src/plugins/alfabank/preferences.test.js
+++ src/plugins/alfabank/preferences.test.js
@@ -1,4 +1,4 @@
-import {normalizeCardExpirationDate} from "./preferences";
+import {normalizeCardExpirationDate, normalizePhoneNumber} from "./preferences";
 
 test("normalizeCardExpirationDate", () => {
     expect(normalizeCardExpirationDate("01/21")).toEqual("01/21");
@@ -7,4 +7,10 @@ test("normalizeCardExpirationDate", () => {
     expect(() => normalizeCardExpirationDate("12")).toThrow("use MM/YY format");
     expect(() => normalizeCardExpirationDate("121")).toThrow("use MM/YY format");
     expect(() => normalizeCardExpirationDate("12121")).toThrow("use MM/YY format");
+});
+
+test("normalizePhoneNumber", () => {
+    expect(normalizePhoneNumber("89234567890")).toEqual("79234567890");
+    expect(normalizePhoneNumber("79234567890")).toEqual("79234567890");
+    expect(normalizePhoneNumber("+79234567890")).toEqual("79234567890");
 });
