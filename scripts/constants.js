f8ea560195e9c23aa54162110d38a138beeb641b
Nikita <dev.nkh@gmail.com>
2018-05-21 21:05:08 +0300

    [CLI] allow `yarn start alfabank` (without specifying PLUGIN= env var), add some tips about ZenMoney.readLine()

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,13 +1,28 @@
 const path = require("path");
 const resolve = relativePath => path.resolve(process.cwd(), relativePath);
 const chalk = require("chalk");
+const fs = require("fs");
+
+const pluginName = process.argv.slice(2).join(" ");
+if (pluginName) {
+    if (process.env.PLUGIN) {
+        console.error(chalk.red(`providing PLUGIN env variable "${process.env.PLUGIN}" together with argv "${pluginName}" is ambiguous, thus prohibited`));
+        process.exit(1);
+    }
+    const candidates = [`src/plugins/${pluginName}`, `src/${pluginName}`, pluginName];
+    const existingCandidate = candidates.find((x) => fs.existsSync(x));
+    if (!existingCandidate) {
+        console.error(chalk.red("Neither of the following folders exists:"));
+        console.error(candidates.map(resolve));
+        console.error(chalk.red(`Make sure "${pluginName}" is a valid existing plugin name or path`));
+        process.exit(1);
+    }
+    process.env.PLUGIN = existingCandidate;
+}
 
 if (!process.env.PLUGIN) {
     const scriptName = path.basename(process.argv.find((x) => x.endsWith(".js")), ".js");
-    const command = process.platform === "win32"
-        ? `set "PLUGIN=src/plugins/example" && yarn ${scriptName}`
-        : `PLUGIN=src/plugins/example yarn ${scriptName}`;
-    console.error(`${chalk.red("You must specify plugin path in PLUGIN env var to proceed, e.g.:")}\n${chalk.cyan(command)}`);
+    console.error(`${chalk.red("You must specify plugin path in PLUGIN env var to proceed, e.g.:")}\n${chalk.cyan(`yarn ${scriptName} example`)}`);
     process.exit(1);
 }
 
