dc6aad696ca0b0c3fda2589c66a57089b509dcfa
skvav <konstantin.sychkin@gmail.com>
2018-05-28 12:03:55 +0300

    ZPAPI.js import fixes

--- src/ZPAPI.js
+++ src/ZPAPI.js
@@ -14,6 +14,7 @@ import {
     setDefaultEncoding,
     setThrowOnError,
 } from "./ZPAPI.utils";
+import {makePluginDataApi} from "./ZPAPI.pluginData";
 
 function sleepSync(durationMs) {
     const startMs = Date.now();
