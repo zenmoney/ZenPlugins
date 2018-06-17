270daf134f62913513d9c8a507c93886b4165d6a
Nikita <dev.nkh@gmail.com>
2018-06-17 11:11:20 +0300

    make syncID sanitizer stricter

--- src/common/accounts.test.js
+++ src/common/accounts.test.js
@@ -1,21 +1,21 @@
-import {convertAccountSyncID} from "./accounts";
+import {ensureSyncIDsAreUniqueButSanitized, maybeSanitizeSyncIdMaybeNot} from "./accounts";
 
-describe("convertAccountSyncID", () => {
-    it("should truncate syncID to last 4 digits if there is no intersection between accounts", () => {
+describe("ensureSyncIDsAreUniqueButSanitized", () => {
+    it("truncate syncID to last4 digits if there is no intersection between accounts", () => {
         const accounts = [
             {syncID: ["0001", "111120"]},
-            {syncID: ["0002", "222"]},
+            {syncID: ["0002", "2222"]},
             {syncID: ["0003"]},
         ];
         const expected = [
             {syncID: ["0001", "1120"]},
-            {syncID: ["0002", "222"]},
+            {syncID: ["0002", "2222"]},
             {syncID: ["0003"]},
         ];
-        expect(convertAccountSyncID(accounts)).toEqual(expected);
+        expect(ensureSyncIDsAreUniqueButSanitized({accounts, sanitizeSyncId: maybeSanitizeSyncIdMaybeNot})).toEqual(expected);
     });
 
-    it("should truncate syncID to last 4 digits if there is intersection between accounts with different instruments", () => {
+    it("truncates syncID to last 4 digits if there is intersection between accounts with different instruments", () => {
         const accounts = [
             {syncID: ["0001", "121120"], instrument: "RUB"},
             {syncID: ["0002", "111120"], instrument: "USD"},
@@ -26,10 +26,10 @@ describe("convertAccountSyncID", () => {
             {syncID: ["0002", "1120"], instrument: "USD"},
             {syncID: ["0003"]},
         ];
-        expect(convertAccountSyncID(accounts)).toEqual(expected);
+        expect(ensureSyncIDsAreUniqueButSanitized({accounts, sanitizeSyncId: maybeSanitizeSyncIdMaybeNot})).toEqual(expected);
     });
 
-    it("should sanitize syncID if there is intersection", () => {
+    it("doesn't trim, but sanitizes syncID to guarantee id uniqueness between accounts with the same instrument", () => {
         const accounts = [
             {syncID: ["0001", "00000121120"], instrument: "RUB"},
             {syncID: ["0002", "00000111120"], instrument: "RUB"},
@@ -40,10 +40,10 @@ describe("convertAccountSyncID", () => {
             {syncID: ["0002", "*****111120"], instrument: "RUB"},
             {syncID: ["0003"]},
         ];
-        expect(convertAccountSyncID(accounts)).toEqual(expected);
+        expect(ensureSyncIDsAreUniqueButSanitized({accounts, sanitizeSyncId: maybeSanitizeSyncIdMaybeNot})).toEqual(expected);
     });
 
-    it("should leave full syncID if there is intersection but syncID are short", () => {
+    it("leaves full syncID if there is intersection but syncID are short", () => {
         const accounts = [
             {syncID: ["0001", "121120"], instrument: "RUB"},
             {syncID: ["0002", "111120"], instrument: "RUB"},
@@ -54,6 +54,6 @@ describe("convertAccountSyncID", () => {
             {syncID: ["0002", "111120"], instrument: "RUB"},
             {syncID: ["0003"]},
         ];
-        expect(convertAccountSyncID(accounts)).toEqual(expected);
+        expect(ensureSyncIDsAreUniqueButSanitized({accounts, sanitizeSyncId: maybeSanitizeSyncIdMaybeNot})).toEqual(expected);
     });
 });
