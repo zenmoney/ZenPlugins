b6b495a7efd38fce4dcd4993ecd028e5b6f44ae3
Nikita <dev.nkh@gmail.com>
2018-06-17 11:11:20 +0300

    make syncID sanitizer stricter

--- src/common/accounts.js
+++ src/common/accounts.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 function getLast4Digits(id, ignoreCurrentSanitizing) {
     id = id.toString().trim();
     if (/\d\d\d\d$/.test(id) && (ignoreCurrentSanitizing || !/[^\d*]/.test(id))) {
@@ -7,11 +9,13 @@ function getLast4Digits(id, ignoreCurrentSanitizing) {
     }
 }
 
-function sanitizeSyncId(id) {
-    id = id.toString().trim();
+export function maybeSanitizeSyncIdMaybeNot(id) {
     return id.length > 9 && !/[^\d*]/.test(id) ? "*****" + id.substring(5) : id;
 }
 
+/**
+ * @deprecated try using stricter ensureSyncIDsAreUniqueButSanitized instead
+ */
 export function convertAccountSyncID(accounts, ignoreCurrentSanitizing) {
     const accountsByLast4Digits = {};
     for (const account of accounts) {
@@ -32,7 +36,7 @@ export function convertAccountSyncID(accounts, ignoreCurrentSanitizing) {
         for (let id of account.syncID) {
             const key = account.instrument + "_" + getLast4Digits(id, ignoreCurrentSanitizing);
             const group = accountsByLast4Digits[key];
-            id = group.length > 1 ? sanitizeSyncId(id) : getLast4Digits(id, ignoreCurrentSanitizing);
+            id = group.length > 1 ? maybeSanitizeSyncIdMaybeNot(id) : getLast4Digits(id, ignoreCurrentSanitizing);
             if (syncID.indexOf(id) < 0) {
                 syncID.push(id);
             }
@@ -42,6 +46,10 @@ export function convertAccountSyncID(accounts, ignoreCurrentSanitizing) {
     return accounts;
 }
 
+/**
+ * @deprecated no kittens will die if you inline _.toPairs(accounts).filter(([key, account]) => key === account.id) in plugin
+ * (this logic is neither generic, nor it is named correctly - it actually filters stuff)
+ */
 export function convertAccountMapToArray(accounts) {
     const filtered = [];
     for (const id in accounts) {
@@ -51,4 +59,35 @@ export function convertAccountMapToArray(accounts) {
         }
     }
     return filtered;
+}
+
+const assertReplacementsAreUnique = (replacementPairs) => {
+    const duplicates = _.toPairs(_.countBy(replacementPairs, ([syncID, replacement]) => replacement))
+        .filter(([replacement, count]) => count !== 1);
+    console.assert(duplicates.length === 0, "invariant: syncID replacementPairs have duplicates", {duplicates});
+};
+
+export function ensureSyncIDsAreUniqueButSanitized({accounts, sanitizeSyncId}) {
+    const replacementsByInstrument = _.mapValues(_.groupBy(accounts, (x) => x.instrument), (accounts, instrument) => {
+        const flattenedAccounts = _.flatMap(accounts, ({syncID, ...rest}) => {
+            console.assert(Array.isArray(syncID), "account.syncID must be array");
+            return syncID.map((singleSyncID) => {
+                console.assert(typeof singleSyncID === "string", "account.syncID must be array of strings, met not a string: " + JSON.stringify(singleSyncID));
+                return ({singleSyncID, ...rest});
+            });
+        });
+        const flattenedAccountsByLast4Digits = _.groupBy(flattenedAccounts, (account) => {
+            console.assert(/\d{4}$/.test(account.singleSyncID), "syncID must end with last4, but was: " + account.singleSyncID);
+            return account.singleSyncID.slice(-4);
+        });
+        const syncIDReplacementPairs = _.flatMap(
+            _.toPairs(flattenedAccountsByLast4Digits),
+            ([last4, flattenedAccountsSharingKey]) => flattenedAccountsSharingKey.length === 1
+                ? [[flattenedAccountsSharingKey[0].singleSyncID, last4]]
+                : flattenedAccountsSharingKey.map((x) => [x.singleSyncID, sanitizeSyncId(x.singleSyncID)]),
+        );
+        assertReplacementsAreUnique(syncIDReplacementPairs);
+        return _.fromPairs(syncIDReplacementPairs);
+    });
+    return accounts.map((account) => ({...account, syncID: account.syncID.map((syncID) => replacementsByInstrument[account.instrument][syncID] || syncID)}));
 }
