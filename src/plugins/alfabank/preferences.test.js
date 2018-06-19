538cf2b312b6551169b4b7ed4bdc31b3f1a6e990
Nikita <dev.nkh@gmail.com>
2018-06-19 20:27:19 +0300

    [alfabank] ask to fill MMYY format instead of MM/YY (simpler)

--- src/plugins/alfabank/preferences.test.js
+++ src/plugins/alfabank/preferences.test.js
@@ -3,10 +3,10 @@ import {normalizeCardExpirationDate, normalizePhoneNumber} from "./preferences";
 test("normalizeCardExpirationDate", () => {
     expect(normalizeCardExpirationDate("01/21")).toEqual("01/21");
     expect(normalizeCardExpirationDate("0121")).toEqual("01/21");
-    expect(() => normalizeCardExpirationDate("")).toThrow("use MM/YY format");
-    expect(() => normalizeCardExpirationDate("12")).toThrow("use MM/YY format");
-    expect(() => normalizeCardExpirationDate("121")).toThrow("use MM/YY format");
-    expect(() => normalizeCardExpirationDate("12121")).toThrow("use MM/YY format");
+    expect(() => normalizeCardExpirationDate("")).toThrow("use MMYY format");
+    expect(() => normalizeCardExpirationDate("12")).toThrow("use MMYY format");
+    expect(() => normalizeCardExpirationDate("121")).toThrow("use MMYY format");
+    expect(() => normalizeCardExpirationDate("12121")).toThrow("use MMYY format");
 });
 
 test("normalizePhoneNumber", () => {
