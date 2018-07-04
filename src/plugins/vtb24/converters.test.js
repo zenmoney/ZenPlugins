72782a9c5e507a11731946c47f1365e20adf763d
skvav <konstantin.sychkin@gmail.com>
2018-07-04 17:57:00 +0300

    [vtb24] convert loan

--- src/plugins/vtb24/converters.test.js
+++ src/plugins/vtb24/converters.test.js
@@ -1,4 +1,4 @@
-import {convertAccount, convertTransaction} from "./converters";
+import {convertAccount, convertLoan, convertTransaction} from "./converters";
 
 describe("convertAccount", () => {
     it("converts account without card", () => {
@@ -358,6 +358,118 @@ describe("convertAccount", () => {
     });
 });
 
+describe("convertLoan", () => {
+    it("converts loan", () => {
+        expect(convertLoan({
+            __type: "ru.vtb24.mobilebanking.protocol.product.LoanContractMto",
+            number: "634/5010-0004540",
+            id: "5D1AB7F045F24C8F823CB785CFDC4ADE",
+            name: "Ипотека",
+            displayName: "Ипотека",
+            showOnMainPage: true,
+            archived: false,
+            issueDate: new Date("Mon Aug 07 2017 00:00:00 GMT+0300"),
+            creditSum:  {
+                __type: "ru.vtb24.mobilebanking.protocol.AmountMto",
+                sum: 1608000,
+                currency:  {
+                    __type: "ru.vtb24.mobilebanking.protocol.CurrencyMto",
+                    currencyCode: "RUR",
+                    name: "Рубль России",
+                    displaySymbol: "₽",
+                },
+            },
+            loan: null,
+            account:  {
+                __type: "ru.vtb24.mobilebanking.protocol.product.LoanAccountMto",
+                number: "43302310200000556732",
+                id: "E7DCC89E89EC47DDA57EB2925D2ECCC1",
+                name: "Ипотека",
+                displayName: "Ипотека",
+                showOnMainPage: true,
+                archived: false,
+                contract:  {
+                    __type: "ru.vtb24.mobilebanking.protocol.product.LoanContractMto",
+                    number: "634/5010-0004540",
+                    id: "5D1AB7F045F24C8F823CB785CFDC4ADE",
+                    name: "Ипотека",
+                    displayName: "Ипотека",
+                    showOnMainPage: true,
+                    archived: false,
+                    issueDate: new Date("Mon Aug 07 2017 00:00:00 GMT+0300"),
+                    creditSum: null,
+                    loan: null,
+                    account: null,
+                    status:  {
+                        __type: "ru.vtb24.mobilebanking.protocol.product.ContractStatusMto",
+                        id: "OPEN",
+                    },
+                    openDate: new Date("Mon Aug 07 2017 00:00:00 GMT+0300"),
+                    endDate: new Date("Thu Oct 07 2032 00:00:00 GMT+0300"),
+                    closeDate: null,
+                    contractPeriod:  {
+                        __type: "ru.vtb24.mobilebanking.protocol.DurationMto",
+                        value: 182,
+                        unit:  {
+                            __type: "ru.vtb24.mobilebanking.protocol.DurationUnitMto",
+                            id: "MONTH",
+                        },
+                    },
+                    details: null,
+                },
+                status:  {
+                    __type: "ru.vtb24.mobilebanking.protocol.product.AccountStatusMto",
+                    id: "OPEN",
+                },
+                openDate: new Date("Mon Aug 07 2017 00:00:00 GMT+0300"),
+                lastOperationDate: null,
+                closeDate: null,
+                amount:  {
+                    __type: "ru.vtb24.mobilebanking.protocol.AmountMto",
+                    sum: 1556772.29,
+                    currency:  {
+                        __type: "ru.vtb24.mobilebanking.protocol.CurrencyMto",
+                        currencyCode: "RUR",
+                        name: "Рубль России",
+                        displaySymbol: "₽",
+                    },
+                },
+                details: null,
+            },
+            status:  {
+                __type: "ru.vtb24.mobilebanking.protocol.product.ContractStatusMto",
+                id: "OPEN",
+            },
+            openDate: new Date("Mon Aug 07 2017 00:00:00 GMT+0300"),
+            endDate: new Date("Thu Oct 07 2032 00:00:00 GMT+0300"),
+            closeDate: null,
+            contractPeriod: null,
+            details: null,
+        })).toEqual({
+            id: "5D1AB7F045F24C8F823CB785CFDC4ADE",
+            type: "ru.vtb24.mobilebanking.protocol.product.LoanContractMto",
+            zenAccount: {
+                id: "5D1AB7F045F24C8F823CB785CFDC4ADE",
+                type: "loan",
+                title: "Ипотека",
+                instrument: "RUB",
+                startDate: new Date("2017-08-07T00:00:00+03:00"),
+                startBalance: 1608000,
+                balance: -1556772.29,
+                percent: 1,
+                capitalization: true,
+                endDateOffsetInterval: "month",
+                endDateOffset: 182,
+                payoffInterval: "month",
+                payoffStep: 1,
+                syncID: [
+                    "43302310200000556732",
+                ],
+            },
+        });
+    });
+});
+
 describe("convertTransaction", () => {
     it("converts transfer", () => {
         expect(convertTransaction({
@@ -415,7 +527,7 @@ describe("convertTransaction", () => {
             },
             transactionDate: new Date("Wed Jun 27 2018 11:45:04 GMT+0300"),
         }, {id: "account"})).toEqual({
-            date: new Date("2018-06-27T11:45:04+0300"),
+            date: new Date("2018-06-27T11:45:04+03:00"),
             hold: true,
             income: 0,
             incomeAccount: "account",
@@ -482,7 +594,7 @@ describe("convertTransaction", () => {
             },
             transactionDate: new Date("Mon Jun 18 2018 19:16:42 GMT+0300"),
         }, {id: "account"})).toEqual({
-            date: new Date("2018-06-18T19:16:42+0300"),
+            date: new Date("2018-06-18T19:16:42+03:00"),
             hold: false,
             income: 0,
             incomeAccount: "account",
