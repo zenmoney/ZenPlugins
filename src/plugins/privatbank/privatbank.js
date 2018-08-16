73975b3e3d962b17f861e5d886c141e57768eba1
skvav <konstantin.sychkin@gmail.com>
2018-08-16 17:21:27 +0300

    [privatbank] handle bad gateway error

--- src/plugins/privatbank/privatbank.js
+++ src/plugins/privatbank/privatbank.js
@@ -43,7 +43,8 @@ export class PrivatBank {
                 "авторизуйтесь в Приват24 и отправьте заявку на вкладке \"Все услуги\" - Бизнес - Мерчант - Заявки.");
         }
         if (response.body) {
-            if (response.status === 504 && response.body.indexOf("504 Gateway Time-out") >= 0) {
+            if ((response.status === 504 && response.body.indexOf("504 Gateway Time-out") >= 0)
+                    || (response.status === 502 && response.body.indexOf("Bad Gateway")) >= 0) {
                 throw new TemporaryError("[NER] Proxy connection error");
             }
             if (response.body.indexOf("invalid signature") >= 0) {
