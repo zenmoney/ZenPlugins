cca215a1c015cc635f59076b2fe8edac69ab0e25
Sergei Cherepanov <sc@connect.club>
2018-09-19 01:55:04 +0300

    Rewrite modulbank plugin to new API

---
+++ src/plugins/modulbank/bank.js
@@ -0,0 +1,55 @@
+import * as network from "../../common/network"
+
+const baseURL = "https://api.modulbank.ru/v1/"
+
+export async function fetchAccounts(token) {
+    ZenMoney.trace("Запрашиваем данные по счетам...")
+    const response = await fetchJson("account-info", {}, token)
+    if (response.status === 401) {
+        // Not Authorized
+        throw new InvalidPreferencesError("Неверный токен авторизации");
+    }
+    const companies = response.body
+    const accounts = []
+    companies.forEach(company => {
+        Array.prototype.push.apply(accounts, company.bankAccounts)
+    })
+    ZenMoney.trace(`Получено счетов: ${accounts.length}`)
+    return accounts
+}
+
+export async function fetchTransactions(token, accounts, fromDate) {
+    ZenMoney.trace(`Запрашиваем операции с ${fromDate.toLocaleString()}`)
+    const allTransactions = []
+    for (let account of accounts) {
+        let skip = 0
+        let transactions
+        do {
+            let response = await fetchJson(
+                `operation-history/${account.id}`,
+                { from: fromDate.toISOString().slice(0, 10), skip },
+                token)
+            if (response.status === 401) {
+                // Not Authorized
+                throw new InvalidPreferencesError("Неверный токен авторизации");
+            }
+            transactions = response.body
+            Array.prototype.push.apply(allTransactions, transactions)
+            skip += transactions.length
+        } while (transactions.length)
+    }
+    ZenMoney.trace(`Получено операций: ${allTransactions.length}`)
+    return allTransactions
+}
+
+async function fetchJson(path, body, token) {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+        },
+        body,
+        sanitizeResponseLog: {headers: {Authorization: true}},
+    }
+    return network.fetchJson(baseURL + path, options)
+}
---
+++ src/plugins/modulbank/index.js
@@ -0,0 +1,29 @@
+//
+// TODO:
+// - Заменить авторизацию по персональному токену, генерируемому в личном кабинете,
+// на авторизацию через токен OAuth.
+// - Добавить поддержку депозитных счетов, счетов для процентов по депозиту, счетов учета резервов.
+//
+import * as bank from "./bank"
+import * as converters from "./converters"
+
+export async function scrape({preferences, fromDate}) {
+    if (!preferences.token) {
+        throw new ZenMoney.Error("Не задан ключ API интернет-банка!", false, true)
+    }
+
+    const accounts = (await bank.fetchAccounts(preferences.token))
+        .map(converters.convertAccount)
+        .filter(account => account && !ZenMoney.isAccountSkipped(account.id))
+    ZenMoney.trace(`Всего счетов: ${accounts.length}`)
+
+    const transactions = (await bank.fetchTransactions(preferences.token, accounts, fromDate))
+        .map(transaction => converters.convertTransaction(transaction, accounts))
+        .filter(transaction => transaction)
+    ZenMoney.trace(`Всего операций: ${transactions.length}`)
+
+    return {
+        accounts,
+        transactions,
+    }
+}
