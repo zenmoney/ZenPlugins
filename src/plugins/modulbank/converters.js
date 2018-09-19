9f4bdc79c8ad77d4ba0e25d707f1135b8b9b92b9
Sergei <s.cherepanov@gmail.com>
2018-09-19 08:07:12 +0300

    [modulbank] Modular API version
    * Rewrite Modulebank plugin
    
    * Rewrite modulbank plugin to new API

---
+++ src/plugins/modulbank/converters.js
@@ -0,0 +1,81 @@
+export function convertAccount(account) {
+    // Расчетный счет
+    if (account.category === "CheckingAccount") {
+        return {
+            id: account.id,
+            title: account.accountName,
+            syncID: [account.number.substring(account.number.length - 4)],
+
+            instrument: convertCurrencyName(account.currency),
+            type: "checking",
+
+            balance: account.balance,
+            startBalance: 0,
+            creditLimit: 0,
+
+            savings: false,
+        }
+    }
+
+    // Дебетовая карта
+    if (account.category === "CardAccount") {
+        return {
+            id: account.id,
+            title: account.accountName,
+            syncID: [account.number.slice(-4)],
+
+            instrument: convertCurrencyName(account.currency),
+            type: "ccard",
+
+            balance: account.balance,
+            startBalance: 0,
+            creditLimit: 0,
+
+            savings: false,
+        }
+    }
+
+    // Неизвестный тип счета
+    ZenMoney.trace(`Cчет ${account.accountName} имеет неподдерживаемый тип ${account.category}`)
+    return null
+}
+
+export function convertTransaction(json, accounts) {
+    if (json.status !== "Executed" && json.status !== "Received") {
+        ZenMoney.trace(`Пропускаем операцию с состоянием '${json.status}'`)
+        return null
+    }
+    if (json.category !== "Debet" && json.category !== "Credit") {
+        ZenMoney.trace(`Пропускаем операцию категории '${json.category}'`)
+        return null
+    }
+
+    const syncID = json.bankAccountNumber.slice(-4)
+    const account = accounts.find(acc => acc.syncID.indexOf(syncID) !== -1)
+    if (!account) {
+        ZenMoney.trace(`Пропускаем операцию c неизвестным счетом 'syncID'`)
+        return null
+    }
+    const transaction = {
+        id: json.id,
+        payee: json.contragentName ?
+            json.contragentName : json.contragentBankAccountNumber,
+        date: new Date(json.executed),
+        incomeAccount: account.id,
+        outcomeAccount: account.id,
+    }
+    if (json.category === "Debet") {
+        transaction.income = json.amountWithCommission || json.amount
+        transaction.opIncomeInstrument = convertCurrencyName(json.currency || account.instrument)
+        transaction.outcome = 0
+    } else if (json.category === "Credit") {
+        transaction.outcome = json.amountWithCommission || json.amount
+        transaction.opOutcomeInstrument = convertCurrencyName(json.currency || account.instrument)
+        transaction.income = 0
+    }
+    return transaction
+}
+
+function convertCurrencyName(currency) {
+    return currency === "RUR" ? "RUB" : currency
+}
