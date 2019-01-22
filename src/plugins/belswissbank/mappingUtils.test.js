554952164c4c941b0027e0be713ee0ff7a4955df
Nikita <dev.nkh@gmail.com>
2019-01-23 00:03:18 +0300

    [belswissbank] adapt to readableTransaction dto

--- src/plugins/belswissbank/mappingUtils.test.js
+++
@@ -1,9 +0,0 @@
-import { formatRate } from './mappingUtils'
-
-describe('formatRate', () => {
-  it('should never output rate below 1', () => {
-    expect(formatRate({ transactionAmount: 50, accountAmount: 50 })).toEqual('1.0000')
-    expect(formatRate({ transactionAmount: 50, accountAmount: 49 })).toEqual('1.0204')
-    expect(formatRate({ transactionAmount: 49, accountAmount: 50 })).toEqual('1/1.0204')
-  })
-})
