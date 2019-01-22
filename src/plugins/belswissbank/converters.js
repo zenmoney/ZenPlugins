8bb643e86fcf719ed146703b316d456b9fb2e720
Nikita <dev.nkh@gmail.com>
2019-01-23 00:03:18 +0300

    [belswissbank] adapt to readableTransaction dto

--- src/plugins/belswissbank/converters.js
+++ src/plugins/belswissbank/converters.js
@@ -1,8 +1,10 @@
+import _ from 'lodash'
+import { addMovement, formatComment, getSingleReadableTransactionMovement, makeCashTransferMovement } from '../../common/converters'
 import { formatCommentDateTime } from '../../common/dateUtils'
+import { mergeTransfers } from '../../common/mergeTransfers'
+import { figureOutAccountRestsDelta, getTransactionFactor, isCashTransferTransaction, isElectronicTransferTransaction, isRejectedTransaction } from './BSB'
 
-export const floorToMinutes = (ticks) => new Date(Math.floor(ticks / 60000) * 60000).valueOf()
-
-export function formatDetails ({ transaction, matchedPayment }) {
+function formatDetails ({ transaction, matchedPayment }) {
   const timestamp = formatCommentDateTime(new Date(transaction.transactionDate))
   if (matchedPayment) {
     return {
@@ -14,4 +16,74 @@ export function formatDetails ({ transaction, matchedPayment }) {
     payee: [transaction.countryCode, transaction.city, transaction.transactionDetails].filter(Boolean).join(' '),
     comment: timestamp
   }
+}
+
+const floorToMinutes = (ticks) => new Date(Math.floor(ticks / 60000) * 60000).valueOf()
+
+export function convertApiTransactionsToReadableTransactions (apiTransactionsByAccount, paymentsArchive) {
+  const paymentsGroupedByTransactionDate = _.groupBy(paymentsArchive, (payment) => floorToMinutes(payment.paymentDate))
+  const items = _.flatMap(apiTransactionsByAccount, ({ apiTransactions, account }) => {
+    return _.sortBy(
+      apiTransactions.filter((transaction) => !isRejectedTransaction(transaction) && transaction.transactionAmount > 0),
+      (x) => x.cardTransactionId
+    )
+      .map((apiTransaction, index, transactions) => {
+        const invoiceSum = getTransactionFactor(apiTransaction) * apiTransaction.transactionAmount
+        const sum = apiTransaction.transactionCurrency === account.instrument
+          ? invoiceSum
+          : figureOutAccountRestsDelta({ transactions, index, accountCurrency: account.instrument })
+        if (sum === null) {
+          console.debug('sum is unknown, ignored transaction', { transaction: apiTransaction })
+          return null
+        }
+        const relevantPayments = paymentsGroupedByTransactionDate[apiTransaction.transactionDate] || []
+        const matchedPayment = relevantPayments.find((payment) =>
+          (apiTransaction.last4.startsWith(payment.last4) || payment.target.slice(0, 4) === apiTransaction.last4.slice(0, 4)) &&
+          payment.currencyIso === apiTransaction.transactionCurrency &&
+          apiTransaction.transactionAmount === payment.amount
+        )
+        const details = formatDetails({ transaction: apiTransaction, matchedPayment })
+        const invoice = account.instrument === apiTransaction.transactionCurrency
+          ? null
+          : { sum: invoiceSum, instrument: apiTransaction.transactionCurrency }
+        const readableTransaction = {
+          movements: [
+            {
+              id: apiTransaction.cardTransactionId.toString(),
+              account: { id: account.id },
+              invoice,
+              sum: sum,
+              fee: 0
+            }
+          ],
+          date: new Date(apiTransaction.transactionDate),
+          hold: null,
+          merchant: { title: details.payee, mcc: null, location: null },
+          comment: _.compact([details.comment, formatComment({ invoice, sum, fee: 0, accountInstrument: account.instrument })]).join('\n') || null
+        }
+        if (isCashTransferTransaction(apiTransaction) && !isElectronicTransferTransaction(apiTransaction)) {
+          return { readableTransaction: addMovement(readableTransaction, makeCashTransferMovement(readableTransaction, account.instrument)), account }
+        }
+        return { readableTransaction, apiTransaction, account }
+      })
+      .filter(Boolean)
+  })
+  return mergeTransfers({
+    items,
+    makeGroupKey: ({ readableTransaction, apiTransaction, account }) => {
+      if (readableTransaction.movements.length > 1) {
+        return null
+      }
+      if (!isElectronicTransferTransaction(apiTransaction)) {
+        return null
+      }
+      const movement = getSingleReadableTransactionMovement(readableTransaction)
+      return [
+        readableTransaction.date.toISOString(),
+        movement.invoice === null ? Math.abs(movement.sum) : Math.abs(movement.invoice.sum),
+        movement.invoice === null ? account.instrument : movement.invoice.instrument
+      ].join('|')
+    },
+    selectReadableTransaction: (x) => x.readableTransaction
+  })
 }
--- src/plugins/belswissbank/index.js
+++ src/plugins/belswissbank/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash'
 import { generateUUID } from '../../common/utils'
-import * as BSB from './BSB'
-import { floorToMinutes, formatDetails } from './converters'
-import { convertToZenMoneyTransaction } from './mappingUtils'
-import { getTransactionToTransferReplacements } from './mergeTransfers'
+import {
+  assertResponseSuccess,
+  authorize,
+  confirm,
+  currencyCodeToIsoCurrency,
+  extractPaymentsArchive,
+  fetchCards,
+  fetchPaymentsArchive,
+  fetchTransactions
+} from './BSB'
+import { convertApiTransactionsToReadableTransactions } from './converters'
 
 async function login ({ deviceId, username, password }) {
-  const authStatus = await BSB.authorize(username, password, deviceId)
+  const authStatus = await authorize(username, password, deviceId)
   switch (authStatus.userStatus) {
     case 'WAITING_CONFIRMATION':
     case 'SMS_WAS_SENT':
@@ -16,7 +23,7 @@ async function login ({ deviceId, username, password }) {
       if (isNaN(retrievedInput)) {
         throw new TemporaryError(`Ожидался числовой код из СМС, а вы ввели ${JSON.stringify(retrievedInput)}`)
       }
-      await BSB.confirm(deviceId, confirmationCode)
+      await confirm(deviceId, confirmationCode)
       break
     case 'OK':
       break
@@ -33,46 +40,9 @@ function convertToZenMoneyAccount (card) {
     title: card.name,
     type: 'ccard',
     syncID: [card.maskedCardNumber.slice(-4)],
-    instrument: BSB.currencyCodeToIsoCurrency(card.currency),
+    instrument: currencyCodeToIsoCurrency(card.currency),
     balance: card.amount
   }
-}
-
-function normalizeBsbTransactions ({ accountCurrency, bsbTransactions, paymentsGroupedByTransactionDate }) {
-  return _.sortBy(
-    bsbTransactions.filter((transaction) => !BSB.isRejectedTransaction(transaction) && transaction.transactionAmount > 0),
-    (x) => x.cardTransactionId
-  )
-    .map((transaction, index, transactions) => {
-      const transactionAmount = BSB.getTransactionFactor(transaction) * transaction.transactionAmount
-      const accountAmount = transaction.transactionCurrency === accountCurrency
-        ? transactionAmount
-        : BSB.figureOutAccountRestsDelta({ transactions, index, accountCurrency })
-      if (accountAmount === null) {
-        console.error('accountAmount is unknown, ignored transaction', { transaction })
-        return null
-      }
-      const relevantPayments = paymentsGroupedByTransactionDate[transaction.transactionDate] || []
-      const matchedPayment = relevantPayments.find((payment) =>
-        (transaction.last4.startsWith(payment.last4) || payment.target.slice(0, 4) === transaction.last4.slice(0, 4)) &&
-        payment.currencyIso === transaction.transactionCurrency &&
-        transaction.transactionAmount === payment.amount
-      )
-      const { payee, comment } = formatDetails({ transaction, matchedPayment })
-      return {
-        transactionId: transaction.cardTransactionId,
-        transactionDate: new Date(transaction.transactionDate),
-        transactionCurrency: transaction.transactionCurrency,
-        transactionAmount,
-        accountCurrency,
-        accountAmount,
-        payee,
-        comment,
-        isCashTransfer: BSB.isCashTransferTransaction(transaction),
-        isElectronicTransfer: BSB.isElectronicTransferTransaction(transaction)
-      }
-    })
-    .filter(Boolean)
 }
 
 const sessionTimeoutMs = 15 * 60 * 1000
@@ -93,39 +63,30 @@ export async function scrape ({ preferences: { username, password }, fromDate, t
   }
   let probe = pluginData.scrapeLastSuccessDate.valueOf() + sessionTimeoutMs > Date.now()
     ? await Promise.all([
-      BSB.fetchCards(),
-      BSB.fetchPaymentsArchive({ fromDate, toDate })
+      fetchCards(),
+      fetchPaymentsArchive({ fromDate, toDate })
     ])
     : null
   if (probe === null || probe.some((x) => x.status === 401)) {
     await login({ deviceId: pluginData.deviceId, username, password })
     probe = await Promise.all([
-      BSB.fetchCards(),
-      BSB.fetchPaymentsArchive({ fromDate, toDate })
+      fetchCards(),
+      fetchPaymentsArchive({ fromDate, toDate })
     ])
   }
   const [cardsResponse, paymentsResponse] = probe
-  BSB.assertResponseSuccess(cardsResponse)
-  const payments = BSB.extractPaymentsArchive(paymentsResponse)
+  assertResponseSuccess(cardsResponse)
+  const paymentsArchive = extractPaymentsArchive(paymentsResponse)
   const accounts = cardsResponse.body
     .filter((card) => !ZenMoney.isAccountSkipped(calculateAccountId(card)))
     .map((card) => convertToZenMoneyAccount(card))
-  const transactionPairs = _.flatten(await Promise.all(accounts.map(async (account) => {
-    const bsbTransactions = await BSB.fetchTransactions(account.id, fromDate, toDate)
-    const bankTransactions = normalizeBsbTransactions({
-      accountCurrency: account.instrument,
-      bsbTransactions,
-      paymentsGroupedByTransactionDate: _.groupBy(payments, (payment) => floorToMinutes(payment.paymentDate))
-    })
-    const zenTransactions = bankTransactions.map((x) => convertToZenMoneyTransaction(account.id, x))
-    return _.zip(bankTransactions, zenTransactions)
-  })))
-  const transactionToTransferReplacements = getTransactionToTransferReplacements(transactionPairs)
+  const apiTransactionsByAccount = _.flatten(await Promise.all(accounts.map(async (account) => ({
+    account,
+    apiTransactions: await fetchTransactions(account.id, fromDate, toDate)
+  }))))
+  const readableTransactions = convertApiTransactionsToReadableTransactions(apiTransactionsByAccount, paymentsArchive)
   return {
     accounts,
-    transactions: _.compact(transactionPairs.map(([bankTransaction, zenTransaction]) => {
-      const replacement = transactionToTransferReplacements[zenTransaction.id]
-      return _.isUndefined(replacement) ? zenTransaction : replacement
-    }))
+    transactions: readableTransactions
   }
 }
