4b79775cdd26b65a49340d9c07e1520da4a7718c
skvav <konstantin.sychkin@gmail.com>
2019-02-20 11:53:25 +0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
 module.exports = {
-    "extends": ["standard", "standard-react"],
-    "globals": {
-        "console": false,
-        "ZenMoney": false,
-        "fetch": false,
-        "TemporaryError": false,
-        "InvalidPreferencesError": false,
-    },
-    "overrides": [
-        {
-            "files": "**/*.test.js",
-            "plugins": ["jest"],
-            "env": {
-                "jest": true,
-            },
-        }
-    ],
-};
+  'extends': ['standard', 'standard-react'],
+  'globals': {
+    'console': false,
+    'ZenMoney': false,
+    'fetch': false,
+    'TemporaryError': false,
+    'InvalidPreferencesError': false
+  },
+  'overrides': [
+    {
+      'files': '**/*.test.js',
+      'plugins': ['jest'],
+      'env': {
+        'jest': true
+      }
+    }
+  ],
+  'rules': {
+    'eqeqeq': ['error', 'always']
+  }
+}
