1173fa5eb6fb54124fadf7e6542f650e2c4ea56b
Slava Shahoika <vistova@gmail.com>
2019-03-29 18:16:10 +0300

    [bnbank] Initial commit.

---
+++ src/plugins/bnbank/index.js
@@ -0,0 +1,22 @@
+import * as bank from './api'
+import * as converters from './converters'
+
+export async function scrape ({ preferences, fromDate, toDate }) {
+  const token = await bank.login(preferences.phone, preferences.password)
+  const accounts = (await bank.fetchAccounts(token))
+  const cards = accounts.cards
+    .map(converters.convertCard)
+    .filter(account => account !== null)
+  const deposits = accounts.deposits
+    .map(converters.convertDeposit)
+    .filter(account => account !== null)
+
+  const preparedAccounts = cards.concat(deposits)
+  const transactions = (await bank.fetchTransactions(token, preparedAccounts, fromDate, toDate))
+    .map(transaction => converters.convertTransaction(transaction, preparedAccounts))
+  console.log(transactions)
+  return {
+    accounts: preparedAccounts,
+    transactions: []
+  }
+}
