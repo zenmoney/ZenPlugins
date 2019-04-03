4ac95675debffc1713199df7a3270b10ce4eca6e
vetheslav <vistova@gmail.com>
2019-04-04 00:16:17 +0300

    [bgpb] Tests for account converting.

--- src/plugins/bgpb/__tests__/converters/accounts.test.js
+++ src/plugins/bgpb/__tests__/converters/accounts.test.js
@@ -3,63 +3,30 @@ import { convertAccount } from '../../converters'
 describe('convertAccount', () => {
   it('should convert checking account', () => {
     const account = convertAccount({
-      'productType': 'PC',
-      'accountId': '1113333',
-      'id': 1,
-      'productCode': '1113333|PC',
-      'show': true,
-      'variationId': '[MASTERCARD][MCW INSTANT BYR 3Y (PAYOKAY)]',
-      'accruedInterest': null,
-      'avlBalance': '99.9',
-      'avlLimit': null,
-      'cardAccounts': [{
-        'accType': null,
-        'accountId': 'BY36MTBK10110008000001111000',
-        'accountIdenType': 'PC',
-        'availableBalance': null,
-        'contractCode': '1113333',
-        'currencyCode': 'BYN',
-        'productType': 'PC'
-      }],
-      'cards': [{
-        'blockReason': null,
-        'cardCurr': 'BYN',
-        'commisDate': '2019-01-31',
-        'commisSum': '99.9',
-        'description': 'MASTERCARD',
-        'embossedName': 'IVAN IVANOV',
-        'isOfAccntHolder': '1',
-        'mainCard': '1',
-        'over': '0',
-        'pan': '999999_1111',
-        'smsNotification': '1',
-        'status': 'A',
-        'term': '01/01',
-        'type': 'MASTERCARD',
-        'vpan': '1234567890123456',
-        'rbs': '1234567',
-        'pinPhoneNumber': '123456789012'
-      }],
-      'description': 'PayOkay',
-      'over': null,
-      'debtPayment': null,
-      'isActive': true,
-      'isOverdraft': false,
-      'rate': '0.0001'
+      'Id': '11161311-117d11',
+      'No': '529911******1111',
+      'ProductType': 'MS',
+      'ProductTypeName': 'Карточка',
+      'Subclass': 'Расчетная карточка &quot;Премиальная&quot; MasterCard World',
+      'Expired': '20230630',
+      'Currency': '840',
+      'CustomName': 'Расчетная карточка',
+      'Icon': 'NFC_MC_BDW',
+      'BankId': 'cb727d83-2d8a-4c54-898f-6d45f0eba713',
+      'ExtraData': 'cb727d83-2d8a-4c54-898f-6d45f0eba713'
     })
 
     expect(account).toEqual({
-      id: '1113333',
+      id: '11161311-117d11',
+      transactionsAccId: null,
       type: 'card',
-      title: 'PayOkay',
-      productType: 'PC',
-      instrument: 'BYN',
-      balance: 99.9,
-      creditLimit: 0,
-      syncID: [
-        'BY36MTBK10110008000001111000',
-        '1111'
-      ]
+      title: 'Расчетная карточка*1111',
+      currencyCode: '840',
+      cardNumber: '529911******1111',
+      instrument: 'USD',
+      balance: 0,
+      syncID: ['1111'],
+      productType: 'MS'
     })
   })
 })
