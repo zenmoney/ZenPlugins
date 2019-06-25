2d6e131c58b72ee58827a80b406cf81e2f9c1339
Nikita Pedorich <pedorich.n@gmail.com>
2019-06-25 22:26:51 +0300

    [epayments] WIP. fetch accounts and transactions added

---
+++ src/plugins/epayments/converters.js
@@ -0,0 +1,108 @@
+import * as tools from './tools'
+
+export function convertCards (cards) {
+  return cards.map(card => {
+    const instrument = resolveInstrument(card.cardCurrency)
+
+    const syncID = [card.id.substr(-4)]
+
+    if (card.hasOwnProperty('prevCardId') && card.prevCardId !== null) {
+      syncID.push(card.prevCardId.substr(-4))
+    } // Скопировано из старого плагина, похоже для перевыпущеных карт
+
+    return {
+      id: card.id,
+      title: 'Карта ' + instrument,
+      type: 'ccard',
+      instrument: instrument,
+      balance: card.balance,
+      startBalance: 0,
+      creditLimit: 0,
+      savings: false,
+      syncID: syncID
+    }
+  })
+}
+
+export function convertWallets (wallets) {
+  var zenWallets = []
+
+  wallets.forEach(wallet => {
+    wallet.balances.forEach(balanceItem => {
+      const accountId = uniqueWalletId(balanceItem.accountNumber, balanceItem.currency)
+
+      zenWallets.push({
+        id: accountId,
+        title: `${balanceItem.accountType} ${balanceItem.accountNumber} (${resolveInstrument(balanceItem.currency)})`,
+        type: 'checking',
+        instrument: resolveInstrument(balanceItem.currency),
+        balance: balanceItem.currentBalance,
+        startBalance: 0,
+        creditLimit: 0,
+        syncID: accountId
+      })
+    })
+  })
+
+  return zenWallets
+}
+
+export function extractAccounts (userInfo) {
+  const cards = convertCards(userInfo.cards)
+  const wallets = convertWallets(userInfo.ewallets)
+
+  return (cards || []).concat(wallets || [])
+}
+
+export function convertTransactions (data) {
+  var zenTransactions = []
+
+  data.forEach(transaction => zenTransactions.push(convertTransaction(transaction)))
+
+  return zenTransactions
+}
+
+export function convertTransaction (transaction) {
+  const zenTransaction = {
+    id: transaction.transactionId.toString(),
+    date: tools.getTimestamp(new Date(Date.parse(transaction.operation.date)).setSeconds(0, 0)),
+    comment: tools.cleanUpText(transaction.details)
+  }
+
+  function getAccountId (transaction) {
+    if (transaction.source.type === 'card') {
+      return uniqueCardId(transaction.source.identity)
+    } else if (transaction.source.type === 'ewallet') {
+      return uniqueWalletId(transaction.source.identity, transaction.currency)
+    }
+  }
+
+  const accountId = getAccountId(transaction)
+
+  zenTransaction.outcomeAccount = accountId
+  zenTransaction.incomeAccount = accountId
+  zenTransaction.outcome = transaction.direction === 'Out' ? 0 : transaction.total
+  zenTransaction.income = transaction.direction === 'In' ? transaction.total : 0
+
+  // TODO: добавить другие типы транзакций?
+  if (transaction.operation.typeCode === 'Atm') { // снятие налички
+    if (transaction.currency !== transaction.txnCurrency) { // Снятие в валюте отличной от валюты счета
+      zenTransaction.income = transaction.txnAmount // Сумма в валюте снятия
+      zenTransaction.incomeAccount = 'cash#' + resolveInstrument(transaction.txnCurrency) // Валюта снятия
+      zenTransaction.outcome = transaction.total // Сумма в валюте счета + комиссия
+      zenTransaction.opOutcome = transaction.txnAmount
+      zenTransaction.opOutcomeInstrument = resolveInstrument(transaction.txnCurrency) // Валюта снятия
+    } else {
+      zenTransaction.income = transaction.amount
+      zenTransaction.incomeAccount = 'cash#' + resolveInstrument(transaction.currency)
+    }
+  }
+
+  return zenTransaction
+}
+
+function resolveInstrument (code) { return code.toUpperCase() }
+
+function uniqueCardId (id) { return id }
+
+function uniqueWalletId (id, currency) { return id + '-' + resolveInstrument(currency) }
--- src/plugins/epayments/index.js
+++ src/plugins/epayments/index.js
@@ -1,11 +1,32 @@
 import * as request from './request'
+import * as converters from './converters'
 
 export async function scrape ({ preferences, fromDate, toDate }) {
   const login = await request.authenthicate(preferences.login, preferences.password)
   console.log(login)
 
+  const authInfo = {
+    tokenType: login.token_type,
+    token: login.access_token
+  }
+
+  const accounts = await request.fetchUserInfo(authInfo).then(response => converters.extractAccounts(response.body))
+  console.log(accounts)
+
+  const transactionsParams = {
+    auth: authInfo,
+    pageParams: {
+      fromDate: fromDate,
+      toDate: toDate
+    }
+  }
+  const transactions = await request.fetchTransactions(transactionsParams.auth, transactionsParams.pageParams)
+    .then(transactions => converters.convertTransactions(transactions))
+
+  console.log(transactions)
+
   return {
-    accounts: [],
-    transactions: []
+    accounts: accounts,
+    transactions: transactions
   }
 }
