8fb453e38ce4aa28ad767247682ef2b843b9abc5
Nikita Pedorich <pedorich.n@gmail.com>
2019-06-26 23:13:36 +0300

    [epayments] New transaction models used. Transaction merger used

--- src/plugins/epayments/converters.js
+++ src/plugins/epayments/converters.js
@@ -1,5 +1,5 @@
 import * as tools from './tools'
-// import { mergeTransfers as commonMergeTransfers } from '../../common/mergeTransfers'
+import { mergeTransfers as commonMergeTransfers } from '../../common/mergeTransfers'
 
 export function convertCards (cards) {
   return cards.map(card => {
@@ -63,79 +63,87 @@ export function convertTransactions (data) {
   return zenTransactions
 }
 
-export function convertTransaction (transaction) {
-  function getAccountId (transaction) {
-    if (transaction.source.type === 'card') {
-      return uniqueCardId(transaction.source.identity)
-    } else if (transaction.source.type === 'ewallet') {
-      return uniqueWalletId(transaction.source.identity, transaction.currency)
+export function convertTransaction (apiTransaction) {
+  function getAccountId (apiTransaction) {
+    if (apiTransaction.source.type === 'card') {
+      return uniqueCardId(apiTransaction.source.identity)
+    } else if (apiTransaction.source.type === 'ewallet') {
+      return uniqueWalletId(apiTransaction.source.identity, apiTransaction.currency)
     }
   }
-
-  const accountId = getAccountId(transaction)
-  const isOutcome = transaction.direction === 'Out'
+  const accountId = getAccountId(apiTransaction)
 
   const zenTransaction = {
-    id: transaction.transactionId.toString(),
-    date: new Date(Date.parse(transaction.operation.date)),
-    comment: tools.cleanUpText(transaction.details),
-    // movements: [ getMovement(transaction, accountId) ],
-    outcomeAccount: accountId,
-    incomeAccount: accountId,
-    outcome: isOutcome ? transaction.total : 0,
-    income: isOutcome ? 0 : transaction.total
-  }
+    movements: [ getMovement(apiTransaction, accountId) ],
+    hold: apiTransaction.state === 'WaitConfirmation',
+    merchant: null, // Нет такой информации в ответе
+    date: new Date(Date.parse(apiTransaction.operation.date)),
+    comment: tools.cleanUpText(apiTransaction.details)
+  };
 
-  // TODO: добавить другие типы транзакций?
-  if (transaction.operation.typeCode === 'Atm') { // снятие налички
-    if (transaction.currency !== transaction.txnCurrency) { // Снятие в валюте отличной от валюты счета
-      zenTransaction.income = transaction.txnAmount // Сумма в валюте снятия
-      zenTransaction.incomeAccount = 'cash#' + resolveInstrument(transaction.txnCurrency) // Валюта снятия
-      zenTransaction.outcome = transaction.total // Сумма в валюте счета + комиссия
-      zenTransaction.opOutcome = transaction.txnAmount
-      zenTransaction.opOutcomeInstrument = resolveInstrument(transaction.txnCurrency) // Валюта снятия
-    } else {
-      zenTransaction.income = transaction.amount
-      zenTransaction.incomeAccount = 'cash#' + resolveInstrument(transaction.currency)
-    }
-  }
+  [ parseCashWithdrawal ].some(parser => parser(apiTransaction, zenTransaction, accountId))
 
   return zenTransaction
 }
 
-// function getMovement (apiTransaction, accountId) {
-//   const isOutcome = apiTransaction.direction === 'Out'
-//   const sum = apiTransaction.total || apiTransaction.amount
-//   const movement = {
-//     id: apiTransaction.transactionId.toString(),
-//     account: { id: accountId },
-//     invoice: null,
-//     sum: isOutcome ? -sum : sum,
-//     fee: apiTransaction.fee || 0
-//   }
-//
-//   if (apiTransaction.txnCurrency && apiTransaction.currency !== apiTransaction.txnCurrency) {
-//     var amount = apiTransaction.txnAmount
-//     if ((movement.sum > 0 && amount < 0) || (movement.sum < 0 && amount > 0)) {
-//       amount *= -1
-//     }
-//     movement.invoice = {
-//       sum: amount,
-//       instrument: resolveInstrument(apiTransaction.txnCurrency)
-//     }
-//   }
-//
-//   return movement
-// }
+function parseCashWithdrawal (apiTransaction, zenTransaction, accountId) {
+  if (apiTransaction.operation.typeCode !== 'Atm') {
+    return false
+  }
 
-// export function mergeTransactions (transactions) {
-//   function absAmount (income, outcome) { return Math.max(Math.abs(income), Math.abs(outcome)) }
-//   function isLoad (data) { return data.comment.toLowerCase().includes('load') }
-//   return commonMergeTransfers({
-//     items: transactions,
-//     makeGroupKey: data => isLoad(data) ? `${absAmount(data.income, data.outcome)}-${data.date.setSeconds(0, 0)}` : null
-//   })
-// }
+  const sum = apiTransaction.total || apiTransaction.amount
+
+  const movement = {
+    id: null,
+    account: {
+      type: 'cash',
+      instrument: resolveInstrument(apiTransaction.txnCurrency),
+      company: null,
+      syncIds: null
+    },
+    invoice: null,
+    sum: -sum,
+    fee: apiTransaction.fee || 0
+  }
+
+  zenTransaction.movements.push(movement)
+
+  return true
+}
+
+function getMovement (apiTransaction, accountId) {
+  const isOutcome = apiTransaction.direction === 'Out'
+  const sum = apiTransaction.total || apiTransaction.amount
+  const movement = {
+    id: apiTransaction.transactionId,
+    account: { id: accountId },
+    invoice: null,
+    sum: isOutcome ? -sum : sum,
+    fee: apiTransaction.fee || 0
+  }
+
+  if (apiTransaction.txnCurrency && apiTransaction.currency !== apiTransaction.txnCurrency) {
+    var amount = apiTransaction.txnAmount
+    if ((movement.sum > 0 && amount < 0) || (movement.sum < 0 && amount > 0)) {
+      amount *= -1
+    }
+    movement.invoice = {
+      sum: amount,
+      instrument: resolveInstrument(apiTransaction.txnCurrency)
+    }
+  }
+
+  return movement
+}
+
+export function mergeTransactions (transactions) {
+  function absAmount (income, outcome) { return Math.max(Math.abs(income), Math.abs(outcome)) }
+  function isLoad (data) { return data.comment.toLowerCase().includes('load') }
+  return commonMergeTransfers({
+    items: transactions,
+    makeGroupKey: data => isLoad(data) ? `${absAmount(data.income, data.outcome)}-${data.date.setSeconds(0, 0)}` : null
+  })
+}
 
 function resolveInstrument (code) { return code.toUpperCase() }
 
