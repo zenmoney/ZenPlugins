0b4cde33069c24d72888848361c577925c9713ed
mikalair <mikalair@outlook.com>
2019-10-25 12:32:19 +0300

    [rrbbank] initial commit

---
+++ src/plugins/rrbbank/api.js
@@ -0,0 +1,150 @@
+import { flatMap, sortBy } from 'lodash'
+import { createDateIntervals as commonCreateDateIntervals } from '../../common/dateUtils'
+import { fetchJson } from '../../common/network'
+import { generateRandomString } from '../../common/utils'
+import { card, deposit } from './converters'
+
+const baseUrl = 'https://ibank.rrb.by/services/v2/'
+
+export function generateDeviceID () { return generateRandomString(16) }
+
+async function fetchApiJson (url, options, predicate = () => true, error = (message) => console.assert(false, message)) {
+  const response = await fetchJson(baseUrl + url, options)
+  if (predicate) {
+    validateResponse(response, response => predicate(response), error)
+  }
+
+  if (response.body && response.body.errorInfo && response.body.errorInfo.error !== '0' && response.body.errorInfo.errorText) {
+    const errorDescription = response.body.errorInfo.errorText
+    const errorMessage = 'Ответ банка: ' + errorDescription
+    if (errorDescription.indexOf('Некорректно введен логин или пароль') >= 0) {
+      throw new InvalidPreferencesError(errorMessage)
+    }
+    throw new TemporaryError(errorMessage)
+  }
+
+  return response
+}
+
+function validateResponse (response, predicate, error) {
+  if (!predicate || !predicate(response)) {
+    error('non-successful response')
+  }
+}
+
+export async function login (login, password) {
+  const deviceID = ZenMoney.getData('device_id') ? ZenMoney.getData('device_id') : generateDeviceID()
+  ZenMoney.setData('device_id', deviceID)
+
+  const res = await fetchApiJson('session/login', {
+    method: 'POST',
+    body: {
+      applicID: '1',
+      clientKind: '5',
+      deviceUDID: deviceID,
+      login: login,
+      password: password
+    },
+    sanitizeRequestLog: { body: { login: true, password: true } }
+  }, response => response.success, message => new InvalidPreferencesError('Неверный логин или пароль'))
+
+  return res.body.sessionToken
+}
+
+export async function fetchAccounts (token) {
+  console.log('>>> Загрузка списка счетов...')
+  const products = (await fetchApiJson('products/getUserAccountsOverview', {
+    method: 'POST',
+    body: {
+      cardAccount: {
+        withBalance: null
+      },
+      corpoCardAccount: {
+        withBalance: null
+      },
+      creditAccount: {},
+      currentAccount: {
+        withBalance: null
+      },
+      depositAccount: {}
+    },
+    headers: { 'session_token': token } }, response => response.body && response.body.overviewResponse && (response.body.overviewResponse.cardAccount || response.body.overviewResponse.depositAccount || response.body.overviewResponse.currentAccount), message => new TemporaryError(message))).body.overviewResponse
+
+  var cards = []
+  if (products.cardAccount) {
+    cards = cards.concat(products.cardAccount)
+  }
+  if (products.corpoCardAccount) {
+    cards = cards.concat(products.corpoCardAccount)
+  }
+  if (products.currentAccount) {
+    cards = cards.concat(products.currentAccount)
+  }
+  return {
+    cards: cards,
+    deposits: products.depositAccount
+  }
+}
+
+export function createDateIntervals (fromDate, toDate) {
+  const interval = 10 * 24 * 60 * 60 * 1000 // 10 days interval for fetching data
+  const gapMs = 1000
+  return commonCreateDateIntervals({
+    fromDate,
+    toDate,
+    addIntervalToDate: date => new Date(date.getTime() + interval - gapMs),
+    gapMs
+  })
+}
+
+export async function fetchTransactions (token, accounts, fromDate, toDate = new Date()) {
+  console.log('>>> Загрузка списка транзакций...')
+  toDate = toDate || new Date()
+
+  const dates = createDateIntervals(fromDate, toDate)
+  const responses = await Promise.all(flatMap(accounts, (account) => {
+    return dates.map(dates => {
+      let url = ''
+      let accountType = ''
+      switch (account.type) {
+        case card:
+          url = 'products/getCardAccountFullStatementWithBlockedAmounts'
+          accountType = '1'
+          break
+        case deposit:
+          url = 'products/getDepositAccountStatement'
+          accountType = '0'
+          break
+        default:
+          return null
+      }
+      return fetchApiJson(url, {
+        method: 'POST',
+        headers: { 'session_token': token },
+        body: {
+          accountType: accountType,
+          cardHash: account.cardHash,
+          currencyCode: account.currencyCode,
+          internalAccountId: account.id,
+          reportData: {
+            from: dates[0].getTime(),
+            till: dates[1].getTime()
+          }
+        }
+      }, response => {
+        response.body.operations = response.body.operations.map(x => { return { accountNumber: account.id, ...x } })
+        return response.body
+      })
+    })
+  }))
+  const operations = flatMap(responses, response => {
+    return flatMap(response.body.operations, d => d)
+  })
+
+  const filteredOperations = sortBy(operations.filter(function (op) {
+    return op.transactionDate > fromDate && op.operationAmount !== 0
+  }), 'operationDate')
+
+  console.log(`>>> Загружено ${filteredOperations.length} операций.`)
+  return filteredOperations
+}
---
+++ src/plugins/rrbbank/converters.js
@@ -0,0 +1,173 @@
+import codeToCurrencyLookup from '../../common/codeToCurrencyLookup'
+export const card = 'card'
+export const deposit = 'deposit'
+
+export function convertCard (json) {
+  return convertAccount(json, card)
+}
+
+export function convertDeposit (json) {
+  return convertAccount(json, deposit)
+}
+
+export function convertAccount (json, accountType) {
+  switch (accountType) {
+    case card:
+      if (json.cards && json.cards.length > 0) { // only loading card accounts
+        const account = {
+          id: json.internalAccountId,
+          type: card,
+          title: json.productName,
+          currencyCode: json.currency,
+          instrument: codeToCurrencyLookup[json.currency],
+          balance: Number.parseFloat(json.cards[0].balance),
+          syncID: [json.internalAccountId],
+          rkcCode: json.rkcCode,
+          cardHash: json.cards[0].cardHash
+        }
+
+        json.cards.forEach(function (el) {
+          account.syncID.push(el.cardNumberMasked.slice(-4))
+        })
+
+        return account
+      }
+      return null
+    case deposit:
+      return {
+        id: json.internalAccountId,
+        type: deposit,
+        title: 'Депозит ' + json.productName,
+        currencyCode: json.currency,
+        instrument: codeToCurrencyLookup[json.currency],
+        balance: Number.parseFloat(json.balanceAmount),
+        syncID: [json.internalAccountId],
+        capitalization: true,
+        startDate: new Date(json.openDate),
+        endDateOffset: (new Date(json.endDate).getTime() - new Date(json.openDate).getTime()) / 1000 / 60 / 60 / 24,
+        endDateOffsetInterval: 'day',
+        percent: json.interestRate,
+        payoffInterval: 'month',
+        payoffStep: 1,
+        rkcCode: json.rkcCode
+      }
+    default:
+      return null
+  }
+}
+
+export function convertTransaction (json, accounts) {
+  const account = accounts.find(account => {
+    console.log(json)
+    return account.syncID.indexOf(json.accountNumber) !== -1
+  })
+
+  const transaction = {
+    date: new Date(json.operationDate),
+    movements: [ getMovement(json, account) ],
+    merchant: null,
+    comment: null,
+    hold: false
+  };
+
+  [
+    parseCash,
+    parseComment,
+    parsePayee
+  ].some(parser => parser(transaction, json))
+
+  return transaction
+}
+
+function getMovement (json, account) {
+  const movement = {
+    id: null,
+    account: { id: account.id },
+    invoice: null,
+    sum: json.operationCode === 3 ? -json.operationAmount : json.operationAmount,
+    fee: 0
+  }
+  if (json.operationName.indexOf('Удержано подоходного налога') >= 0) {
+    let nameSplit = json.operationName.split(' ')
+    movement.fee = Number.parseFloat(nameSplit[nameSplit.length - 1])
+  }
+  return movement
+}
+
+function parseCash (transaction, json) {
+  if (json.operationName.indexOf('наличных') > 0 ||
+    json.operationName.indexOf('Снятие денег со счета') > 0 ||
+    json.operationName.indexOf('Пополнение наличными') > 0) {
+    // добавим вторую часть перевода
+    transaction.movements.push({
+      id: null,
+      account: {
+        company: null,
+        type: 'cash',
+        instrument: json.trans_iso_currency ? json.trans_iso_currency : codeToCurrencyLookup[json.operationCurrency],
+        syncIds: null
+      },
+      invoice: null,
+      sum: Number.parseFloat(json.auth_amount ? json.auth_amount : json.operationAmount),
+      fee: 0
+    })
+    return true
+  }
+}
+
+function parsePayee (transaction, json) {
+  // интернет-платежи отображаем без получателя
+  if (!json.operationPlace ||
+    json.operationPlace.indexOf('BNB - OPLATA USLUG') >= 0 ||
+    json.operationPlace.indexOf('OPLATA USLUG - KOMPLAT BNB') >= 0) {
+    return false
+  }
+  transaction.merchant = {
+    mcc: null,
+    location: null
+  }
+  const merchant = json.operationPlace.split('>').map(str => str.trim())
+  if (merchant.length === 1) {
+    transaction.merchant.fullTitle = json.operationPlace
+  } else if (merchant.length === 2) {
+    transaction.merchant.title = merchant[0]
+    let geo = merchant[1].split(' ')
+    transaction.merchant.city = merchant[1].replace(' ' + geo[geo.length - 1], '').trim()
+    transaction.merchant.country = geo[geo.length - 1]
+  } else {
+    throw new Error('Ошибка обработки транзакции с получателем: ' + json.operationPlace)
+  }
+}
+
+function parseComment (transaction, json) {
+  if (!json.operationName) { return false }
+
+  if (json.operationName.indexOf('Капитализация. Удержано подоходного налога') >= 0) {
+    transaction.comment = 'Капитализация'
+    return false
+  }
+  switch (json.operationName) {
+    // переводы между счетами полезной информации не несут, гасим сразу
+    case 'Списание по операции ПЦ "Перечисление с карты на карту" ':
+    case 'On-line пополнение договора (списание с БПК)':
+      return true
+
+    // в покупках комментарии не оставляем
+    case 'Покупки':
+    case 'Покупки(конверсия)':
+    case 'Операция в Интернет-Банк':
+    case 'Покупка товаров и услуг':
+    case 'Оплата товаров и услуг':
+    case 'Списание по операции ПЦ "Оплата услуг в ИБ" ':
+      return false
+
+    default:
+      transaction.comment = json.operationName
+      return false
+  }
+}
+
+export function getLastTransactionDate (str) {
+  const [day, month, year, hour, minute, second] = str.match(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2}):(\d{2})/).slice(1)
+  return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}+03:00`)
+}
