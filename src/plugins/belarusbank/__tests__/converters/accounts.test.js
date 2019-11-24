8bc3e3720b6080db7bee1b20f972271c82e90b08
Slava Shahoika <vistova@gmail.com>
2019-11-24 11:47:24 +0300

    [belarusbank] Remove unneeded currency correction

--- src/plugins/belarusbank/__tests__/converters/accounts.test.js
+++ src/plugins/belarusbank/__tests__/converters/accounts.test.js
@@ -52,55 +52,6 @@ describe('convertAccount', () => {
       }
     },
     {
-      name: 'card with wrong currency',
-      json: {
-        balance: '10.18',
-        cardNum: '1111********2222',
-        cardName: 'Карта',
-        currency: 'USD на дату 08.10.2019',
-        id: 'BY75 AKBB 3014 1000 0222 3303 0000',
-        transactionsData: {
-          action: 'long/long/link',
-          additional: [
-            '\'viewns_Z7_HASH001_:ClientCardsDataForm\'',
-            '\'viewns_Z7_HASH001_:ClientCardsDataForm:accountContainer:0:ns_Z7_HASH001_j_id4\'',
-            '\'accountNumber\'',
-            '\'500011001111\''
-          ],
-          encodedURL: '/wps/myportal/ibank/!ut/p/a1/LONGLONGHASH/dl5/d5/HASH56/pw/Z7_HASH001/res/id=pagesQCPclientCardsQCPclientCardList.xhtml/c=cacheLevelPage/=/',
-          viewState: '/s/BLA/BLA/BLA+bla+bLa+Blabla/ha12sh='
-        },
-        type: 'card'
-      },
-      expectedAccount: {
-        id: 'BY75AKBB30141000022233030000',
-        type: 'card',
-        title: 'Карта 2222',
-        balance: 10.18,
-        instrument: 'USD',
-        syncID: ['BY75AKBB30141000022233030000', '2222'],
-        raw: {
-          balance: '10.18',
-          cardNum: '1111********2222',
-          cardName: 'Карта',
-          currency: 'USD на дату 08.10.2019',
-          id: 'BY75 AKBB 3014 1000 0222 3303 0000',
-          transactionsData: {
-            action: 'long/long/link',
-            additional: [
-              '\'viewns_Z7_HASH001_:ClientCardsDataForm\'',
-              '\'viewns_Z7_HASH001_:ClientCardsDataForm:accountContainer:0:ns_Z7_HASH001_j_id4\'',
-              '\'accountNumber\'',
-              '\'500011001111\''
-            ],
-            encodedURL: '/wps/myportal/ibank/!ut/p/a1/LONGLONGHASH/dl5/d5/HASH56/pw/Z7_HASH001/res/id=pagesQCPclientCardsQCPclientCardList.xhtml/c=cacheLevelPage/=/',
-            viewState: '/s/BLA/BLA/BLA+bla+bLa+Blabla/ha12sh='
-          },
-          type: 'card'
-        }
-      }
-    },
-    {
       name: 'deposit',
       json: {
         balance: '1 280.00',
