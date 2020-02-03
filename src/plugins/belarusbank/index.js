ec647fecba5925b09eabf6c49c87e1764e2d8403
ilyaGitHub34 <57803710+ilyaGitHub34@users.noreply.github.com>
2020-02-03 09:59:50 +0300

    [belarusbank] fix raw data deletion

--- src/plugins/belarusbank/index.js
+++ src/plugins/belarusbank/index.js
@@ -7,7 +7,6 @@ export async function scrape ({ preferences, fromDate, toDate }) {
   const cards = (await bank.fetchCards(accountURLs.cards))
     .map(converters.convertAccount)
     .filter(account => account !== null)
-  cards.forEach(card => delete card.raw)
 
   const transactionsCard = (await bank.fetchCardsTransactions(cards[0]))
     .map(transaction => converters.convertTransaction(transaction, cards))
@@ -15,6 +14,7 @@ export async function scrape ({ preferences, fromDate, toDate }) {
   const deposits = (await bank.fetchDeposits(accountURLs.deposits))
     .map(converters.convertAccount)
     .filter(account => account !== null)
+  cards.forEach(card => delete card.raw)
   return {
     accounts: cards.concat(deposits),
     transactions: transactionsCard
