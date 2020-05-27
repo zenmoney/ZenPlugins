e12e30876411bf8e336bd9e837c034ea1375a562
Slava Shahoika <vistova@gmail.com>
2020-05-27 22:55:50 +0300

    [solutionbank] Do not update account deposit if there is no transactions

--- src/plugins/solutionbank/index.js
+++ src/plugins/solutionbank/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 import { login, fetchAccounts, fetchTransactionsMini, fetchTransactions } from './api'
 import { convertAccount, convertTransaction, transactionsUnique } from './converters'
 
-export async function scrape ({ preferences, fromDate, toDate }) {
+export async function scrape ({ preferences, fromDate, toDate, isFirstRun }) {
   const token = await login(preferences.login, preferences.password)
   const accounts = (await fetchAccounts(token))
     .map(convertAccount)
@@ -15,7 +15,24 @@ export async function scrape ({ preferences, fromDate, toDate }) {
     .map(transaction => convertTransaction(transaction))
     .filter(transaction => transaction !== null)
   return {
-    accounts: accounts,
+    accounts: removeBalances(accounts, transactions, isFirstRun),
     transactions: _.sortBy(transactions, transaction => transaction.date)
   }
+}
+
+function removeBalances (accounts, transactions, isFirstRun) {
+  if (isFirstRun) {
+    return accounts
+  }
+  const map = []
+  _.flatMap(transactions, (tr) => {
+    map[tr.movements[0].account.id] = true
+  })
+
+  for (let i = 0; i < accounts.length; i++) {
+    if (map[accounts[i].id] === undefined) {
+      accounts[i].balance = null
+    }
+  }
+  return accounts
 }
