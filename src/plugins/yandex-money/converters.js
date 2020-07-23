e0ce846ab8a56ce08f7910ef72d46490ceac46f8
svvdg <66954710+svvdg@users.noreply.github.com>
2020-07-23 15:37:44 +0300

    Additional write-off for the operation

--- src/plugins/yandex-money/converters.js
+++ src/plugins/yandex-money/converters.js
@@ -15,7 +15,6 @@ export function convertAccount (apiAccount) {
   } else {
     account.balance = apiAccount.balance
   }
-  console.log('____??????????____')
   return account
 }
 
@@ -30,7 +29,6 @@ export function convertTransaction (apiTransaction, account) {
     sum: apiTransaction.direction === 'in' ? apiTransaction.amount : -apiTransaction.amount,
     instrument: apiTransaction.amount_currency || account.instrument
   }
-  console.log('____??????????____')
   const transaction = {
     date: new Date(apiTransaction.datetime),
     hold: false,
@@ -51,7 +49,6 @@ export function convertTransaction (apiTransaction, account) {
     parseYandexMoneyTransfer,
     parsePayee
   ].some(parser => parser(apiTransaction, transaction))
-  console.log(transaction)
   return transaction
 }
 
@@ -64,20 +61,17 @@ function parseYandexMoneyTransfer (apiTransaction, transaction) {
     /Перевод от (\d+)/i
   ]) {
     const match = apiTransaction.title.match(pattern)
-    console.log('____??????????____')
     if (match) {
-      transaction.merchant = { // YM 4100148118398
+      transaction.merchant = {
         country: null,
         city: null,
         title: `YM ${match[1]}`,
         mcc: (transaction.merchant && transaction.merchant.mcc) || null,
         location: null
       }
-      console.log('____??????????____')
       return true
     }
   }
-  console.log('____??????????____')
   return false
 }
 
@@ -89,10 +83,8 @@ function parseMcc (apiTransaction, transaction) {
         ...(transaction.merchant || {}),
         mcc: parseInt(match[1], 10)
       }
-      console.log('____??????????____')
     }
   }
-  console.log('____??????????____')
   return false
 }
 
@@ -110,32 +102,28 @@ function parsePayee (apiTransaction, transaction) {
       /Благодарность проекту [«"]?([^"»]*)["»]?/i,
       /Оплата услуг (.*)/i
     ]
-  console.log('____??????????____')
   for (const pattern of patterns) {
     const match = apiTransaction.title.match(pattern)
     if (match) {
-      transaction.merchant = { // 6,542541865986110009   5,Скрытый смысл
+      transaction.merchant = {
         country: null,
         city: null,
         title: match[1],
         mcc: (transaction.merchant && transaction.merchant.mcc) || null,
         location: null
       }
-      console.log('____??????????____')
       return false
     }
   }
-  if (apiTransaction.direction === 'in' || [ // 4,Дополнительное зачисление по операции   9,Сбербанк, пополнение   10,travelpayouts.ru, пополнение   11,Пополнение с банковской карты
+  if (apiTransaction.direction === 'in' || [
     /Пополнение счета (.*)/i
   ].some(pattern => apiTransaction.title.match(pattern))) {
     transaction.comment = apiTransaction.title
-    console.log('____??????????____')
-  } else if (apiTransaction.direction === 'out' && [ // 3,Дополнительное списание по операции
+  } else if (apiTransaction.direction === 'out' && [
     /Дополнительное (.*)/i
   ].some(pattern => apiTransaction.title.match(pattern))) {
     transaction.comment = apiTransaction.title
-    console.log('____??????????____')
-  } else if (transaction.merchant && [6011].includes(transaction.merchant.mcc)) { // 1,Снятие наличных в банкомате: VB24 D. 15, LIT. G, PR
+  } else if (transaction.merchant && [6011].includes(transaction.merchant.mcc)) {
     // mcc 6011 - cash withdrawal
     transaction.merchant = null
     transaction.comment = apiTransaction.title
@@ -153,8 +141,7 @@ function parsePayee (apiTransaction, transaction) {
         fee: 0
       }
     )
-    console.log('____??????????____')
-  } else { // 2,541029695812341276   7,PP*2649CODE mcc: 8999  ( 8,SHOP "EVROOPT" MVV mcc: 5411 sum: null ?????
+  } else {
     transaction.merchant = {
       country: null,
       city: null,
@@ -162,7 +149,5 @@ function parsePayee (apiTransaction, transaction) {
       mcc: (transaction.merchant && transaction.merchant.mcc) || null,
       location: null
     }
-    console.log('____??????????____')
   }
-  console.log('____??????????____')
 }
