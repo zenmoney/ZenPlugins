1ec3d1116e909fcfd09f095821f6fce441d9e149
svvdg <66954710+svvdg@users.noreply.github.com>
2020-07-29 11:11:52 +0300

    [yandex-money]_Tests_v1

--- src/plugins/yandex-money/__tests__/transactions/cashWithdrawal.test.js
+++ src/plugins/yandex-money/__tests__/transactions/cashWithdrawal.test.js
@@ -22,7 +22,7 @@ describe('convertTransaction', () => {
         movements: [
           {
             id: '599249979205221162',
-            account: { id: 'account' }, // 'account' ???
+            account: { id: 'account' },
             invoice: null,
             sum: -10000,
             fee: 0
@@ -47,67 +47,3 @@ describe('convertTransaction', () => {
     expect(convertTransaction(apiTransaction, account)).toEqual(transaction)
   })
 })
-
-/*
-
-// const toReadableTransactionForAccount = account => transaction => convertTransaction(transaction, account)
-
-describe('convertTransaction', () => {
-  const account = {
-    id: 'account',
-    instrument: 'RUB'
-  }
-
-  it('converts cash withdrawal', () => {
-    const apiTransactions = [
-      {
-        operation_id: '599249979205221162',
-        title: 'Снятие наличных в банкомате: VB24 D. 15, LIT. G, PR',
-        amount: 10000,
-        direction: 'out',
-        datetime: '2018-12-27T18:19:39Z',
-        status: 'success',
-        type: 'payment-shop',
-        group_id: 'mcc_6011',
-        spendingCategories: [{ name: 'TransferWithdraw', sum: 10000 }]
-      }
-    ]
-
-    const expectedReadableTransactions = [
-      {
-        date: new Date('2018-12-27T18:19:39.000Z'),
-        hold: false,
-        comment: 'Снятие наличных в банкомате: VB24 D. 15, LIT. G, PR',
-        merchant: null,
-        movements: [
-          {
-            id: '599249979205221162',
-            account: { id: 'account' },
-            invoice: null,
-            sum: -10000,
-            fee: 0
-          },
-          {
-            id: null,
-            account: {
-              type: 'cash',
-              instrument: 'RUB',
-              syncIds: null,
-              company: null
-            },
-            invoice: null,
-            sum: 10000,
-            fee: 0
-          }
-        ]
-      }
-    ]
-
-    const toReadableTransaction = toReadableTransactionForAccount(account)
-    const readableTransactions = apiTransactions.map(toReadableTransaction)
-    expect(readableTransactions).toEqual(expectedReadableTransactions)
-
-  })
-})
-
- */
--- src/plugins/yandex-money/__tests__/transactions/transfer.test.js
+++ src/plugins/yandex-money/__tests__/transactions/transfer.test.js
@@ -48,7 +48,12 @@ describe('convertTransaction', () => {
           }
         ]
       }
-    ],
+    ]
+  ])('converts transfer to Yandex Money wallet', (apiTransaction, transaction) => {
+    expect(convertTransaction(apiTransaction, account)).toEqual(transaction)
+  })
+
+  it.each([
     [
       {
         pattern_id: '4601',
@@ -93,7 +98,7 @@ describe('convertTransaction', () => {
         ]
       }
     ]
-  ])('converts transfer to Yandex Money wallet', (apiTransaction, transaction) => {
+  ])('converts transfer to card', (apiTransaction, transaction) => {
     expect(convertTransaction(apiTransaction, account)).toEqual(transaction)
   })
 
@@ -129,113 +134,3 @@ describe('convertTransaction', () => {
     }, account)).toBeNull()
   })
 })
-
-/*
-
-const toReadableTransactionForAccount = account => transaction => convertTransaction(transaction, account)
-
-describe('convertTransaction', () => {
-  const account = {
-    id: 'account',
-    instrument: 'RUB'
-  }
-
-  it('converts transfer to Yandex Money wallet', () => {
-    const apiTransactions = [
-      {
-        pattern_id: 'p2p',
-        operation_id: '550751409179120010',
-        title: 'Перевод на счет 4100148118398',
-        amount: 100.00,
-        direction: 'out',
-        datetime: '2017-06-14T10:30:12Z',
-        status: 'success',
-        type: 'outgoing-transfer',
-        group_id: 'type_history_p2p_outgoing_all'
-      },
-      {
-        pattern_id: '4601',
-        group_id: 'pattern_4601',
-        operation_id: '645418930452788631',
-        title: 'Перевод на карту 553691******2743',
-        amount: 450.82,
-        direction: 'out',
-        datetime: '2020-06-14T15:02:13Z',
-        status: 'success',
-        type: 'payment-shop',
-        showcase_format: 'json',
-        spendingCategories: [{ name: 'TransferWithdraw', sum: 450.82 }],
-        amount_currency: 'RUB',
-        is_sbp_operation: false
-      }
-    ]
-
-    const expectedReadableTransactions = [
-      {
-        date: new Date('2017-06-14T10:30:12.000Z'),
-        hold: false,
-        comment: null,
-        merchant: {
-          country: null,
-          city: null,
-          title: 'YM 4100148118398',
-          mcc: null,
-          location: null
-        },
-        movements: [
-          {
-            id: '550751409179120010',
-            account: { id: 'account' },
-            invoice: null,
-            sum: -100,
-            fee: 0
-          },
-          {
-            id: null,
-            account: {
-              type: null,
-              instrument: 'RUB',
-              syncIds: ['4100148118398'],
-              company: null
-            },
-            invoice: null,
-            sum: 100,
-            fee: 0
-          }
-        ]
-      },
-      {
-        date: new Date('2020-06-14T15:02:13.000Z'),
-        hold: false,
-        comment: 'Перевод на карту 553691******2743',
-        merchant: null,
-        movements: [
-          {
-            id: '645418930452788631',
-            account: { id: 'account' },
-            invoice: null,
-            sum: -450.82,
-            fee: 0
-          },
-          {
-            id: null,
-            account: {
-              type: null,
-              instrument: 'RUB',
-              syncIds: ['2743'],
-              company: null
-            },
-            invoice: null,
-            sum: 450.82,
-            fee: 0
-          }
-        ]
-      }
-    ]
-
-    const toReadableTransaction = toReadableTransactionForAccount(account)
-    const readableTransactions = apiTransactions.map(toReadableTransaction)
-    expect(readableTransactions).toEqual(expectedReadableTransactions)
-  })
-
- */
