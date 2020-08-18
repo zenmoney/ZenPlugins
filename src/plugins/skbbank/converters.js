8dda464c24ff887c2f97b3fd1bc4b986be8f6fb9
Slava Yozhkin <yozhkin@gmail.com>
2020-08-18 19:31:43 +0300

    [skbbank] first version

---
+++ src/plugins/skbbank/converters.js
@@ -0,0 +1,117 @@
+export function convertCard (json) {
+  if (json.category !== 'card') {
+    console.log('Unexpected category ' + json.category)
+    return null
+  }
+  return {
+    id: json.primaryAccount,
+    storedId: json.storedId.toString(), // На этот id приходят платежи при стягивании.
+    type: 'ccard',
+    title: json.name, // 'Mastercard Unembossed'
+    instrument: json.currency, // 'RUB'
+    balance: json.availableBalance, // <= json.balance
+    creditLimit: 0,
+    syncID: [json.pan.slice(-4)]
+  }
+}
+
+export function convertAccount (json) {
+  if (json.type !== 'current') {
+    return null
+  }
+  return {
+    id: json.number,
+    type: 'checking',
+    title: json.name, // 'Счет RUB'
+    balance: json.balance,
+    instrument: json.currency,
+    creditLimit: 0,
+    syncID: [json.number.slice(-4)]
+  }
+}
+
+export function convertDeposit (json) {
+  const payoffInterval = {
+    'В конце срока': null
+  }[json.percentPaidPeriod]
+  if (payoffInterval === undefined) {
+    console.log('Unexpected percentPaidPeriod ' + json.percentPaydPeriod)
+    return null
+  }
+  let payoffStep = 1
+  if (payoffInterval === null) {
+    payoffStep = 0
+  }
+  return {
+    id: json.account,
+    type: 'deposit',
+    title: json.name,
+    instrument: json.currency,
+    balance: json.balance,
+    capitalization: json.capitalization,
+    percent: json.rate,
+    startDate: json.open_date,
+    endDateOffset: Number(json.duration),
+    endDateOffsetInterval: 'day',
+    payoffInterval: payoffInterval,
+    payoffStep: payoffStep,
+    syncID: [json.account.slice(-4)]
+  }
+}
+
+function findAccountByStoredId (accounts, storedId) {
+  for (const acc of accounts) {
+    if (acc.storedId === storedId) {
+      return acc
+    }
+  }
+  console.assert(false, 'cannot find storedId ' + storedId)
+}
+
+export function convertTransaction (json, accounts) {
+  const transaction = {
+    id: json.info.id.toString(),
+    date: json.view.dateCreated,
+    hold: json.view.state !== 'processed'
+  }
+  // Для операции стягивания в productAccount будет null, в productCardId - идентификатор внешней карты.
+  let accountId = json.view.productAccount
+  if (!accountId && json.info.operationType === 'payment') {
+    accountId = findAccountByStoredId(accounts, json.details['payee-card']).id
+  }
+  transaction.incomeAccount = transaction.outcomeAccount = accountId
+  transaction.income = transaction.outcome = 0
+  if (json.view.direction === 'debit') {
+    transaction.outcome = json.view.amounts.amount
+    transaction.payee = json.view.descriptions.operationDescription
+    if (json.view.amounts.currency !== 'RUB' && json.info.operationType === 'card_transaction') {
+      transaction.opOutcome = json.view.amounts.amount
+      transaction.opOutcomeInstrument = json.view.amounts.currency
+      transaction.outcome = json.details.convAmount
+    }
+  } else if (json.view.direction === 'credit') {
+    transaction.income = json.view.amounts.amount
+    transaction.comment = json.view.descriptions.operationDescription || json.view.mainRequisite
+    transaction.opIncome = json.view.amounts.amount
+    transaction.opIncomeInstrument = json.view.amounts.currency
+  } else if (json.view.direction === 'internal') {
+    transaction.income = json.view.amounts.amount
+    transaction.outcome = json.view.amounts.amount
+    transaction.comment = json.view.descriptions.operationDescription || json.view.mainRequisite
+    if (json.info.operationType === 'account_transaction') {
+      transaction.outcomeAccount = json.details.payeeAccount
+    } else if (json.info.operationType === 'payment') {
+      transaction.incomeAccount = json.details['payer-account']
+      transaction.outcomeAccount = json.details['payee-account']
+    }
+  } else {
+    console.assert(false, 'unexpected transaction direction ' + json.view.direction)
+  }
+  if (json.view.mainRequisite) {
+    const mcc = json.view.mainRequisite.match('МСС: (\\d+)')
+    if (mcc) {
+      transaction.mcc = Number(mcc[1])
+    }
+  }
+  return transaction
+}
