61f3d812dd88c43547dcc9cd7fb3ef26ba29db62
svvdg <66954710+svvdg@users.noreply.github.com>
2020-08-20 15:58:18 +0300

    1234

---
+++ src/plugins/kapitalbank-uz/__tests__/transactions/outerOutcomeTransfer.test.js
@@ -0,0 +1,10 @@
+// import {
+  // convertUzcardCardTransaction,
+  // convertHumoCardTransaction,
+  // convertVisaCardTransaction
+  // convertWalletTransaction,
+  // convertAccountTransaction
+// } from '../../converters'
+
+// describe('convertTransaction', () => {
+  // it.each([
