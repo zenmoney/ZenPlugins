7346e93cfc4ba1eed380cc70ca69a78dc1d6b570
Konstantin Sychkin <konstantin.sychkin@gmail.com>
2020-10-21 16:21:37 +0300

    [skbbank] throw if plugin is in background

--- src/plugins/skbbank/index.js
+++ src/plugins/skbbank/index.js
@@ -1,7 +1,11 @@
+import { UserInteractionError } from '../../errors'
 import { fetchProducts, fetchTransactions, login } from './api'
 import { convertAccounts, convertTransaction, groupAccountsById } from './converters'
 
-export async function scrape ({ preferences, fromDate, toDate }) {
+export async function scrape ({ preferences, fromDate, toDate, isInBackground }) {
+  if (isInBackground) {
+    throw new UserInteractionError()
+  }
   await login(preferences.login, preferences.password)
   const products = await fetchProducts()
   const accounts = convertAccounts(products)
