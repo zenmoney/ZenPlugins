aaaf7775c55776405e40a7da9c52f4ce01ae676d
Anton Karpov <karpoff@bk.ru>
2021-03-21 13:28:56 +0400

    [ineco] add debug info

--- src/plugins/ineco/index.js
+++ src/plugins/ineco/index.js
@@ -14,6 +14,8 @@ const formatNumber = value => {
 const formatDate = date => `${formatNumber(date.getDate())}/${formatNumber(date.getMonth() + 1)}/${date.getFullYear()}`
 
 const scrapeAccountTransactions = async (ah, account, accountKey, session, fromDate, toDate) => {
+  console.log(`fetching transactions for ${account.id} (${accountKey}) savings account`, { fromDate, toDate })
+
   const response = await session.postForm('/AccountStatement/Export', {
     export_filter: `${formatDate(fromDate)};${formatDate(toDate)};${accountKey};3`,
     export_sorting: '',
@@ -24,6 +26,8 @@ const scrapeAccountTransactions = async (ah, account, accountKey, session, fromD
 }
 
 const scrapeCardTransactions = async (ah, account, accountKey, session, fromDate, toDate) => {
+  console.log(`fetching transactions for ${account.id} (${accountKey}) card account`, { fromDate, toDate })
+
   const response = await session.postForm('/CardStatement/Export', {
     export_filter: `${formatDate(fromDate)};${formatDate(toDate)};${accountKey};3`,
     export_sorting: '',
@@ -53,11 +57,15 @@ export async function scrape ({ preferences, fromDate, toDate }) {
   const accounts = parseAccounts(html)
   const ah = new AccountHelper(accounts)
 
+  console.log(`got ${accounts.length}`, { accounts })
+
   for (const account of accounts) {
     const response = await session.request(`/Account/GetStatement?id=${account.id.slice(account.id.length - 11)}&accountType=0`, { redirect: 'manual' })
     const accountKey = (response.headers.location || '').split('/').pop()
 
     if (!accountKey) {
+      console.log(`scrapping account ${account.id} is skipped because of no key`, { redirectUrl: response.headers.location })
+
       continue
     }
 
