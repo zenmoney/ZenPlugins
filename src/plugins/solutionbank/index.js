ec14770998d5047ac9288b080a9f33865bc98285
Alexey <knazty@gmail.com>
2021-05-23 18:29:18 +0400

    Fix some sync issues

--- src/plugins/solutionbank/index.js
+++ src/plugins/solutionbank/index.js
@@ -1,38 +1,19 @@
 import _ from 'lodash'
-import { login, fetchAccounts, fetchTransactionsMini, fetchTransactions } from './api'
-import { convertAccount, convertTransaction, transactionsUnique } from './converters'
+import { login, fetchAccounts, fetchOperations, fetchTransactions } from './api'
+import { convertAccount, convertTransaction, merge } from './converters'
 
-export async function scrape ({ preferences, fromDate, toDate, isFirstRun }) {
+export async function scrape ({ preferences, fromDate, toDate }) {
   const token = await login(preferences.login, preferences.password)
   const accounts = (await fetchAccounts(token))
     .map(convertAccount)
     .filter(account => account !== null)
-  const transactions = transactionsUnique(
-    (await fetchTransactionsMini(token, accounts, fromDate, toDate)).concat(
-      await fetchTransactions(token, accounts, fromDate, toDate)
-    )
-  )
+  const transactions = merge(
+    await fetchTransactions(token, accounts, fromDate),
+    await fetchOperations(token, accounts, fromDate, toDate))
     .map(transaction => convertTransaction(transaction))
     .filter(transaction => transaction !== null)
   return {
-    accounts: removeBalances(accounts, transactions, isFirstRun),
+    accounts: accounts,
     transactions: _.sortBy(transactions, transaction => transaction.date)
   }
-}
-
-function removeBalances (accounts, transactions, isFirstRun) {
-  if (isFirstRun) {
-    return accounts
-  }
-  const map = []
-  _.flatMap(transactions, (tr) => {
-    map[tr.movements[0].account.id] = true
-  })
-
-  for (let i = 0; i < accounts.length; i++) {
-    if (map[accounts[i].id] === undefined) {
-      accounts[i].balance = null
-    }
-  }
-  return accounts
 }
