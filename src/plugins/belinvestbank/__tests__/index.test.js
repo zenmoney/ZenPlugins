b509e9a17d852c58c73364cef79af26f109a1e28
Konstantin Sychkin <konstantin.sychkin@gmail.com>
2021-06-25 16:51:27 +0300

    [belinvestbank] update app version

--- src/plugins/belinvestbank/__tests__/index.test.js
+++ src/plugins/belinvestbank/__tests__/index.test.js
@@ -389,7 +389,7 @@ function mockApiLoginAndPass () {
       login: '123456789',
       password: 'pass',
       deviceId: 'device id',
-      versionApp: '2.1.35',
+      versionApp: '2.1.38',
       os: 'Android',
       device_token: 'device token',
       device_token_type: 'ANDROID'
