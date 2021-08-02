816f4e6cf4f22dd373eb74461e73b91a774a0494
Konstantin Sychkin <konstantin.sychkin@gmail.com>
2021-08-02 14:51:58 +0300

    fix calculating day difference in getIntervalBetweenDates

--- src/common/momentDateUtils.js
+++ src/common/momentDateUtils.js
@@ -6,7 +6,9 @@ export function getIntervalBetweenDates (fromDate, toDate, intervals = ['year', 
   const [d2, m2, y2] = toAdjustedDayMonthYearTuple(toDate)
   for (let i = 0; i < intervals.length; i++) {
     const interval = intervals[i]
-    const count = moment(toDate).diff(moment(fromDate), interval, i < intervals.length - 1)
+    const count = interval === 'day'
+      ? Math.floor((toDate.getTime() - fromDate.getTime()) / (24 * 3600 * 1000))
+      : moment(toDate).diff(moment(fromDate), interval, i < intervals.length - 1)
     if (_.isInteger(count)) {
       return { interval, count }
     }
