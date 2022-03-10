d005217e2d5d00aca5fa7cb274d6c035e855c519
EvilWind <evilwind@protonmail.com>
2022-03-11 02:37:10 +0300

    improve typing sanitize

---
+++ src/common/sanitize.d.ts
@@ -0,0 +1,5 @@
+export declare function sanitize (value: unknown, mask: unknown): unknown
+
+export declare function sanitizeUrl (url: string, mask: unknown): string
+
+export declare function sanitizeUrlContainingObject (object: unknown, mask: unknown): unknown
