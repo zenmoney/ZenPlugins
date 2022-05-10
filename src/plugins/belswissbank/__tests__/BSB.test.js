1d02f62d8e420dd92531bc60b136cab84a6cbef5
Nikita <dev.nkh@gmail.com>
2022-05-10 19:36:58 +0300

    Revert "[belswissbank] rm no longer used figureOutAccountRestsDelta"
    This reverts commit de5b94a61b97d2f766d73e28851667029f2d10a1.

--- src/plugins/belswissbank/__tests__/BSB.test.js
+++ src/plugins/belswissbank/__tests__/BSB.test.js
@@ -1,4 +1,4 @@
-import { formatBsbSmsApiDate, formatBsbArchiveApiDate, isRejectedTransaction } from '../BSB'
+import { figureOutAccountRestsDelta, formatBsbSmsApiDate, formatBsbArchiveApiDate, isRejectedTransaction } from '../BSB'
 
 describe('formatBsbCardsApiDate', () => {
   it('should return DD.MM.YYYY string considering bank UTC+3 timezone shift', () => {
@@ -33,5 +33,56 @@ describe('formatBsbPaymentsApiDate', () => {
 describe('isRejectedTransaction', () => {
   it('should trim corrupted transactionType', () => {
     expect(isRejectedTransaction({ transactionType: 'Отказ ' })).toBe(true)
+  })
+})
+
+describe('figureOutAccountRestsDelta', () => {
+  it('should do its best in aggressive environment with null accountRest', () => {
+    const transactions = [
+      { accountRest: 900, transactionCurrency: 'USD', transactionAmount: 100, transactionType: 'Товары и услуги', colour: 2 },
+      { accountRest: null, transactionCurrency: 'USD', transactionAmount: 200, transactionType: 'Товары и услуги', colour: 2 },
+      { accountRest: null, transactionCurrency: 'USD', transactionAmount: 300, transactionType: 'Товары и услуги', colour: 2 },
+      { accountRest: 0, transactionCurrency: 'USD', transactionAmount: 400, transactionType: 'Товары и услуги', colour: 2 },
+      { accountRest: null, transactionCurrency: 'USD', transactionAmount: 1000, transactionType: 'Зачисление', colour: 1 },
+      { accountRest: 750, transactionCurrency: 'EUR', transactionAmount: 200, transactionType: 'Товары и услуги', colour: 2 },
+      { accountRest: null, transactionCurrency: 'EUR', transactionAmount: 300, transactionType: 'Товары и услуги', colour: 2 },
+      { accountRest: 0, transactionCurrency: 'USD', transactionAmount: 400, transactionType: 'Товары и услуги', colour: 2 }
+    ]
+    const accountCurrency = 'USD'
+    expect(figureOutAccountRestsDelta({ index: 0, transactions, accountCurrency })).toBe(null)
+    expect(figureOutAccountRestsDelta({ index: 1, transactions, accountCurrency })).toBe(-200)
+    expect(figureOutAccountRestsDelta({ index: 2, transactions, accountCurrency })).toBe(-300)
+    expect(figureOutAccountRestsDelta({ index: 3, transactions, accountCurrency })).toBe(-400)
+    expect(figureOutAccountRestsDelta({ index: 4, transactions, accountCurrency })).toBe(1000)
+    expect(figureOutAccountRestsDelta({ index: 5, transactions, accountCurrency })).toBe(-250)
+    expect(figureOutAccountRestsDelta({ index: 6, transactions, accountCurrency })).toBe(null)
+    expect(figureOutAccountRestsDelta({ index: 7, transactions, accountCurrency })).toBe(null)
+  })
+
+  it('should check that index is in range', () => {
+    const transactions = [
+      { accountRest: 900, transactionCurrency: 'USD', transactionAmount: 100, transactionType: 'Товары и услуги' }
+    ]
+    const accountCurrency = 'USD'
+    expect(() => figureOutAccountRestsDelta({ index: -1, transactions, accountCurrency })).toThrow()
+    expect(() => figureOutAccountRestsDelta({ index: 1, transactions, accountCurrency })).toThrow()
+  })
+
+  it('should handle ambiguous amounts signs', () => {
+    const transactions = [
+      {
+        transactionType: 'Товары и услуги',
+        transactionAmount: 1,
+        transactionCurrency: 'BYN',
+        accountRest: 11.46
+      },
+      {
+        transactionType: 'Товары и услуги',
+        transactionAmount: 10,
+        transactionCurrency: 'BYN',
+        accountRest: 27.93
+      }
+    ]
+    expect(figureOutAccountRestsDelta({ index: 1, transactions, accountCurrency: 'USD' })).toBeNull()
   })
 })
