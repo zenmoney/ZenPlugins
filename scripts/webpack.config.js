35df3b6a7a8855d9e059a3624d070cb5ee1bd5a9
EvilWind <evilwind@protonmail.com>
2022-07-14 14:21:53 +0300

    use relative websocket path

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -135,8 +135,7 @@ function generatePluginConfig (production, server, pluginName, outputPath) {
         devMiddleware: {
           publicPath: '/'
         },
-        host: 'localhost', // available on your dev machine only
-        // host: 'local-ip', // available on local network — use with caution: opens security hole allowing credentials leak
+        host: 'localhost',
         port: 'auto',
         webSocketServer: WebsocketServer,
 
