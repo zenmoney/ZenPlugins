fc3a58a7ca2cbb44c526ab897ed4548827fbeda1
Konstantin Sychkin <konstantin.sychkin@gmail.com>
2023-04-07 17:49:03 +0300

    restrict common error constructor typing

--- src/errors.d.ts
+++ src/errors.d.ts
@@ -16,15 +16,25 @@ export declare class TemporaryError extends ZPAPIError {
   constructor (message?: string)
 }
 
-export declare class IncompatibleVersionError extends TemporaryError {}
+export declare class IncompatibleVersionError extends TemporaryError {
+  constructor ()
+}
 
-export declare class TemporaryUnavailableError extends TemporaryError {}
+export declare class TemporaryUnavailableError extends TemporaryError {
+  constructor ()
+}
 
-export declare class InvalidLoginOrPasswordError extends InvalidPreferencesError {}
+export declare class InvalidLoginOrPasswordError extends InvalidPreferencesError {
+  constructor ()
+}
 
-export declare class InvalidOtpCodeError extends TemporaryError {}
+export declare class InvalidOtpCodeError extends TemporaryError {
+  constructor ()
+}
 
-export declare class PinCodeInsteadOfPasswordError extends InvalidPreferencesError {}
+export declare class PinCodeInsteadOfPasswordError extends InvalidPreferencesError {
+  constructor ()
+}
 
 export declare class BankMessageError extends TemporaryError {
   bankMessage: string
@@ -32,10 +42,14 @@ export declare class BankMessageError extends TemporaryError {
   constructor (bankMessage: string, forcePluginReinstall?: boolean)
 }
 
-export declare class PreviousSessionNotClosedError extends TemporaryError {}
+export declare class PreviousSessionNotClosedError extends TemporaryError {
+  constructor ()
+}
 
 export declare class UserInteractionError extends TemporaryError {
   constructor ()
 }
 
-export declare class PasswordExpiredError extends InvalidPreferencesError {}
+export declare class PasswordExpiredError extends InvalidPreferencesError {
+  constructor ()
+}
