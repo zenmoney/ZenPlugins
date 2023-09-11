658ffd5aefbddb2ddb8a9c1bcbf7647fc9fb1cd8
Konstantin Sychkin <konstantin.sychkin@gmail.com>
2023-09-11 18:50:31 +0300

    consider error caused by user input as fatal on first run

--- src/errors.d.ts
+++ src/errors.d.ts
@@ -25,11 +25,11 @@ export declare class TemporaryUnavailableError extends TemporaryError {
 }
 
 export declare class InvalidLoginOrPasswordError extends InvalidPreferencesError {
-  constructor ()
+  constructor (message?: string)
 }
 
 export declare class InvalidOtpCodeError extends TemporaryError {
-  constructor ()
+  constructor (message?: string)
 }
 
 export declare class PinCodeInsteadOfPasswordError extends InvalidPreferencesError {
