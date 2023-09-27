242800c95d54cb9463cc53f1e9e5a598508908a8
Konstantin Sychkin <konstantin.sychkin@gmail.com>
2023-09-27 19:21:24 +0300

    [tronscan] fix code style

--- src/plugins/tronscan/api.ts
+++ src/plugins/tronscan/api.ts
@@ -2,8 +2,9 @@ import qs from 'querystring'
 import { fetchJson, FetchOptions, FetchResponse } from '../../common/network'
 import get from '../../types/get'
 import { SUPPORTED_TOKENS } from './config'
+import { delay } from '../../common/utils'
 
-const MAX_RPS = 3;
+const MAX_RPS = 3
 
 export interface Preferences {
   wallets: string
@@ -27,66 +28,61 @@ export interface TokenTransfer {
 }
 
 export class TronscanApi {
-  private readonly baseUrl: string;
-  private activeList: Array<Promise<unknown>> = [];
+  private readonly baseUrl: string
+  private activeList: Array<Promise<unknown>> = []
 
-  constructor (options: {baseUrl: string}) {
+  constructor (options: { baseUrl: string }) {
     this.baseUrl = options.baseUrl
   }
 
-   private async fetchApi(
+  private async fetchApi (
     url: string,
     options?: FetchOptions,
     predicate?: (x: FetchResponse) => boolean
   ): Promise<FetchResponse> {
     if (this.activeList.length < MAX_RPS) {
+      const request = this.fetchInner(url, options, predicate)
 
-		const request = this.fetchInner(url, options, predicate);
+      const waiter = request
+        .then(async () => await delay(1000))
+        .catch(async () => await delay(1000))
+        .then(() => {
+          this.activeList = this.activeList.filter(item => item !== waiter)
+        })
 
-		const waiter = request
-		  .then(async () => await this.delay(1000))
-		  .catch(async () => await this.delay(1000))
-		  .then(() => {
-			this.activeList = this.activeList.filter(item => item !== waiter);
-		  });
+      this.activeList.push(waiter)
 
-		this.activeList.push(waiter);
-		
-		const result = await request
+      const result = await request
 
-		return result
-	}
+      return result
+    }
 
-	await Promise.race(this.activeList);
-    return await this.fetchApi(url, options, predicate);
+    await Promise.race(this.activeList)
+    return await this.fetchApi(url, options, predicate)
   }
 
-  private async fetchInner(
+  private async fetchInner (
     url: string,
     options?: FetchOptions,
     predicate?: (x: FetchResponse) => boolean
   ): Promise<FetchResponse> {
-    const response = await fetchJson(this.baseUrl + url, options);
+    const response = await fetchJson(this.baseUrl + url, options)
 
     if (predicate) {
       this.validateResponse(
         response,
         response => !get(response.body, 'error') && predicate(response)
-      );
+      )
     }
 
-    return response;
+    return response
   }
 
-  private validateResponse(
+  private validateResponse (
     response: FetchResponse,
     predicate?: (x: FetchResponse) => boolean
   ): void {
-    console.assert(!predicate || predicate(response), 'non-successful response');
-  }
-
-  private delay(ms: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    console.assert(!predicate || predicate(response), 'non-successful response')
   }
 
   public async fetchTokens (wallet: string): Promise<TokenInfo[]> {
@@ -97,7 +93,7 @@ export class TronscanApi {
       })}`,
       undefined,
       (res) => typeof res.body === 'object' && res.body != null && 'data' in res.body
-    ) as FetchResponse & {body: {data: Array<Record<string, unknown>>}}
+    ) as FetchResponse & { body: { data: Array<Record<string, unknown>> } }
 
     return response.body.data
       .filter(t => SUPPORTED_TOKENS.includes(t.tokenAbbr as string))
@@ -117,14 +113,14 @@ export class TronscanApi {
 
     while (true) {
       const response = await this.fetchApi(
-    `token_trc20/transfers?${qs.stringify({
-      relatedAddress: wallet,
-      start_timestamp: fromDate.valueOf(),
-      end_timestamp: toDate?.valueOf(),
-      limit,
-      start,
-      sort: '-timestamp'
-    })}`) as FetchResponse & {body: {total: number, token_transfers: Array<Record<string, unknown>>}}
+        `token_trc20/transfers?${qs.stringify({
+          relatedAddress: wallet,
+          start_timestamp: fromDate.valueOf(),
+          end_timestamp: toDate?.valueOf(),
+          limit,
+          start,
+          sort: '-timestamp'
+        })}`) as FetchResponse & { body: { total: number, token_transfers: Array<Record<string, unknown>> } }
 
       transfers.push(...response.body.token_transfers.map(t => ({
         transaction_id: t.transaction_id as string,
