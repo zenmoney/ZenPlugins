a516ab757b74c66a07c3c53c86b14594f5c86ced
sergeykochanov <47022537+sergeykochanov@users.noreply.github.com>
2024-01-29 10:33:02 +0100

    [raiffeisen-rs] Add initial support for Raiffeisen Bank Serbia (#712)

---
+++ src/plugins/raiffeisen-rs/converters.ts
@@ -0,0 +1,136 @@
+import { AccountType, Transaction } from '../../types/zenmoney'
+import { AccountBalanceResponse, GetAccountTransactionsResponse, RaiffAccount } from './models'
+import moment from 'moment'
+
+export function convertAccounts (apiAccounts: AccountBalanceResponse[]): RaiffAccount[] {
+  const accounts: RaiffAccount[] = []
+  for (const apiAccount of apiAccounts) {
+    const res = convertAccount(apiAccount)
+    if (res != null) {
+      accounts.push(res)
+    }
+  }
+  return accounts
+}
+
+function convertAccount (apiAccount: AccountBalanceResponse): RaiffAccount | null {
+  // Add CurrencyCode for multicurrency accounts
+  const id = apiAccount.AccountNumber + apiAccount.CurrencyCode
+  const account: RaiffAccount = {
+    id,
+    type: AccountType.ccard,
+    title: apiAccount.Description ?? id,
+    instrument: apiAccount.CurrencyCode,
+    balance: apiAccount.AvailableBalance,
+    creditLimit: 0, // TODO: add credit card support
+    syncIds: [id],
+    ProductCodeCore: apiAccount.ProductCodeCore
+  }
+  return account
+}
+
+export function convertTransaction (t1: GetAccountTransactionsResponse): Transaction {
+  const description = t1.Description
+  const details = t1.Details
+  const invoice = details.c_CurrencyCode_tx !== null &&
+    details.c_CurrencyCode_tx.length > 0 &&
+    details.c_CurrencyCode_tx !== details.s_CurrencyCode
+    ? {
+        sum: -details.c_Amount_tx!,
+        instrument: details.c_CurrencyCode_tx
+      }
+    : null
+  return {
+    hold: false,
+    date: t1.Details.c_Date_tx !== null && t1.Details.c_Date_tx.length > 0
+      ? new Date(t1.Details.c_Date_tx)
+      : new Date(t1.ProcessedDate),
+    movements: [
+      {
+        id: t1.TransactionID ?? null,
+        account: { id: t1.AccountNumber + t1.CurrencyCode },
+        invoice,
+        sum: t1.CreditAmount - t1.DebitAmount,
+        fee: 0
+      }
+    ],
+    merchant: description != null
+      ? {
+          fullTitle: description,
+          mcc: null,
+          location: null
+        }
+      : null,
+    comment: description !== details.s_Note_st ? details.s_Note_st : null
+  }
+}
+
+export function convertTransfer (t1: GetAccountTransactionsResponse, t2: GetAccountTransactionsResponse): Transaction {
+  const description = t1.Description
+  return {
+    hold: false,
+    date: t1.Details.c_Date_tx !== null && t1.Details.c_Date_tx.length > 0
+      ? new Date(t1.Details.c_Date_tx)
+      : new Date(t1.ProcessedDate),
+    movements: [
+      {
+        id: t1.TransactionID ?? null,
+        account: { id: t1.AccountNumber + t1.CurrencyCode },
+        invoice: null,
+        sum: t1.CreditAmount - t1.DebitAmount,
+        fee: 0
+      },
+      {
+        id: t2.TransactionID ?? null,
+        account: { id: t2.AccountNumber + t2.CurrencyCode },
+        invoice: null,
+        sum: t2.CreditAmount - t2.DebitAmount,
+        fee: 0
+      }
+    ],
+    merchant: description != null
+      ? {
+          fullTitle: description,
+          mcc: null,
+          location: null
+        }
+      : null,
+    comment: description !== t1.Details.s_Note_st ? t1.Details.s_Note_st : null
+  }
+}
+
+export function convertTransactionInProgress (transaction: string[], accountNumber: string): Transaction {
+  enum RESPONSE_FIELDS {
+    FIELD_1 = 0,
+    DATE,
+    DESCRIPTON,
+    AMOUNT,
+    CURRENCY_CODE,
+    CURRENCY_CODE_NUM,
+    FIELD_6,
+    FIELD_7,
+    FIELD_8
+  }
+  const description = transaction[RESPONSE_FIELDS.DESCRIPTON]
+  return {
+    hold: true,
+    date: moment(transaction[RESPONSE_FIELDS.DATE], 'DD.MM.YYYY hh:mm:ss').toDate(),
+    movements: [
+      {
+        id: null,
+        account: { id: accountNumber + transaction[RESPONSE_FIELDS.CURRENCY_CODE] },
+        invoice: null,
+        sum: -transaction[RESPONSE_FIELDS.AMOUNT],
+        fee: 0
+      }
+    ],
+    merchant: description != null
+      ? {
+          fullTitle: description,
+          mcc: null,
+          location: null
+        }
+      : null,
+    comment: null
+  }
+}
---
+++ src/plugins/raiffeisen-rs/models.ts
@@ -0,0 +1,101 @@
+import { AccountOrCard } from '../../types/zenmoney'
+
+// Stored in persistent storage
+export interface Auth {
+  cookie: string
+}
+
+// Input preferences from schema in preferences.xml
+export interface Preferences {
+  login: string
+  password: string
+}
+
+export interface RaiffAccount extends AccountOrCard {
+  ProductCodeCore: string
+}
+
+export interface LoginResponse {
+  Username: string
+  Ticket: string
+  SocialIdentityNumber: string
+  FirstName: string
+  LastName: string
+  FullName: string
+}
+
+export interface AccountBalanceResponse {
+  CurrencyCode: string
+  AccountType: string
+  IsEbankingAccount: string
+  LastChangeAmount: number
+  ID: number
+  CurrencyCodeNumeric: string
+  LastChangeType: string
+  LastChangeDate: string
+  BlockedAmount: number | null
+  AccountCustomName: string | null
+  SubsystemProductID: number | null
+  fwStatus: number
+  IsCASRegistrationForbidden: number
+  IBANNumber: string | null
+  fwNamespace: string
+  ProductOrder: string | null
+  Alias: string | null
+  AccountID: number
+  BansExist: number
+  Balance: number
+  AccountNumber: string
+  IsOwner: boolean
+  Description: string | null
+  Blocked: string
+  FPAccountAllowed: boolean
+  ProductCodeCore: string
+  AvailableBalance: number
+  fwType: string
+}
+
+export interface GetAccountTransactionsResponse {
+  CurrencyCode: string
+  TransactionID: string
+  ProcessedDate: string
+  CurrencyCodeNumeric: number
+  ChequeCardNumber: string | null
+  CreditAmount: number
+  TransactionBeneficiary: string
+  AccountNumber: string
+  DebitAmount: number
+  Reference: string
+  Description: string
+  Note: string
+  ValueDate: string
+  TransactionType: string
+  Details: GetTransactionDetailsResponse
+  [others: string]: string | number | null | GetTransactionDetailsResponse
+}
+
+export interface GetTransactionDetailsResponse {
+  s_Note_st: string
+  c_CurrencyCode_tx: string | null
+  c_CalculationDate: string | null
+  c_CardNumber: string | null
+  s_ValueDate: string
+  s_ProcessedDate: string
+  s_CurrencyCodeNumeric: number
+  c_DomesticAmount: number | null
+  s_OrderNumber: string
+  c_TerminalID: string | null
+  c_DebtorDetails: string | null
+  DebtorAccount: string | null
+  m_DebtorAmount: number | null
+  c_AuthorizationID: string | null
+  s_CurrencyCode: string
+  c_Reference: number | null
+  s_PaymentBasis: number
+  s_Group: string
+  c_Date_tx: string | null
+  s_ConfirmationDate: string
+  s_Amount: number
+  c_Amount_tx: number | null
+  [others: string]: string | number | null
+}
