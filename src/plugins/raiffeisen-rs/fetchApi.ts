b14481ea653a520562a8d8b1d6bb2bdca34cffbb
Sergey Kochanov <sergey.kochanov@outlook.com>
2024-02-05 18:31:07 +0100

    [Raiffeisen-RS] Try to use WebView for password encoding

--- src/plugins/raiffeisen-rs/fetchApi.ts
+++ src/plugins/raiffeisen-rs/fetchApi.ts
@@ -1,18 +1,21 @@
-import { fetchJson, FetchOptions, FetchResponse } from '../../common/network'
-import { InvalidLoginOrPasswordError } from '../../errors'
+import { fetch, fetchJson, FetchOptions, FetchResponse, openWebViewAndInterceptRequest } from '../../common/network'
+import { InvalidLoginOrPasswordError, UserInteractionError } from '../../errors'
 import { parse, splitCookiesString } from 'set-cookie-parser'
 import { AccountBalanceResponse, Auth, GetAccountTransactionsResponse, GetTransactionDetailsResponse, LoginResponse, Preferences } from './models'
 import { isArray } from 'lodash'
 import * as argon2 from 'argon2-browser'
 import moment from 'moment'
 
-const baseUrl = 'https://rol.raiffeisenbank.rs/Retail/Protected/Services/'
+const SALTED_PASSWORD_KEY = 'saltedPassword'
+const API_URL = 'https://rol.raiffeisenbank.rs/Retail/Protected/Services/'
+
+const isWebAssemblyAvailable = 'WebAssembly' in global
 
 async function fetchApi (url: string, options?: FetchOptions): Promise<FetchResponse> {
-  return await fetchJson(baseUrl + url, options ?? {})
+  return await fetchJson(API_URL + url, options ?? {})
 }
 
-async function getSaltedPassword (login: string, password: string): Promise<string> {
+async function getSaltedPasswordProgrammatically (login: string, password: string): Promise<string> {
   const salt = login.padEnd(8, '\0')
   const saltedPassword = await argon2.hash({
     pass: password,
@@ -25,12 +28,45 @@ async function getSaltedPassword (login: string, password: string): Promise<stri
   return saltedPassword.hashHex
 }
 
+async function getSaltedPasswordFromUser (login: string): Promise<string> {
+  // Тeoретически, сюда можно передать и пароль в формате base64_urlsafe,
+  // но кажется, что это не ОК с точки зрения использования данных пользователя.
+  const PASSWORD_ENCODE_URL = 'https://cyberchef.org/#recipe=Argon2(%7B\'option\':\'UTF8\',\'string\':\'' +
+    login + '\'%7D,3,4096,1,32,\'Argon2i\',\'Hex%20hash\')&input=UGFzc3dvcmRDaGFuZ2VNZQ'
+
+  await openWebViewAndInterceptRequest({
+    url: PASSWORD_ENCODE_URL,
+    intercept: (request) => {
+      return true
+    }
+  })
+
+  const saltedPassword = await ZenMoney.readLine('Зашифрованный пароль из формы')
+  if (!saltedPassword) {
+    throw new UserInteractionError()
+  }
+  return saltedPassword
+}
+
+async function getSaltedPassword (login: string, password: string): Promise<string> {
+  const savedPassword = ZenMoney.getData(SALTED_PASSWORD_KEY)
+  if (savedPassword) {
+    return savedPassword as string
+  }
+
+  const saltedPassword = isWebAssemblyAvailable ? await getSaltedPasswordProgrammatically(login, password) : await getSaltedPasswordFromUser(login)
+  ZenMoney.setData(SALTED_PASSWORD_KEY, saltedPassword)
+  ZenMoney.saveData()
+
+  return saltedPassword
+}
+
 export async function fetchAuthorization ({ login, password }: Preferences): Promise<Auth> {
-  const isPasswordSalted = (await fetchApi('RetailLoginService.svc/SaltedPassword', {
+  const isPasswordSalted = (await fetch(API_URL + 'RetailLoginService.svc/SaltedPassword', {
     method: 'POST',
-    body: {
+    body: JSON.stringify({
       userName: login
-    },
+    }),
     sanitizeRequestLog: {
       body: {
         userName: true
@@ -39,7 +75,7 @@ export async function fetchAuthorization ({ login, password }: Preferences): Pro
     sanitizeResponseLog: {
       headers: { 'set-cookie': true }
     }
-  })).body === true
+  })).body === 'true'
 
   const response = await fetchApi('RetailMobileLoginService.svc/Login', {
     method: 'POST',
@@ -74,6 +110,7 @@ export async function fetchAuthorization ({ login, password }: Preferences): Pro
   }) as FetchResponse & { body: LoginResponse, headers: { 'set-cookie': string } }
 
   if (response.body.Ticket === null || response.body.Ticket.length === 0) {
+    ZenMoney.clearData()
     throw new InvalidLoginOrPasswordError()
   }
 
