a81d261cdab318307dc95d3a7bf0882cc127da33
sergeykochanov <47022537+sergeykochanov@users.noreply.github.com>
2024-02-19 10:04:48 +0100

    [postal-savings-rs] Fix empty title for some operations (#721)

--- src/plugins/postal-savings-rs/__tests__/converters/transactions/payment.test.ts
+++ src/plugins/postal-savings-rs/__tests__/converters/transactions/payment.test.ts
@@ -20,7 +20,35 @@ describe('payment', () => {
    '<td align="right" width="100" nowrap>-2,56</td>\n' +
    '<td align="right" width="40" nowrap>00000</td>\n' +
    '<td align="right" width="100" nowrap>25.703,41</td>\n' +
+'</tr>\n' +
+'<tr>\n' +
+    '<td align="right" width="80" nowrap>25.01.2024</td>\n' +
+  '<td align="left" width="130" nowrap>ISPLATA VISA&nbsp;</td>\n' +
+  '<td align="right" width="100" nowrap>EUR 978</td>\n' +
+  '<td align="right" width="100" nowrap>-378,85</td>\n' +
+  '<td align="right" width="40" nowrap>00000</td>\n' +
+  '<td align="right" width="100" nowrap>20.655,86</td>\n' +
 '</tr>')).toEqual([{
+      comment: null,
+      date: new Date('2024-01-25T00:00:00.000'),
+      hold: false,
+      merchant: {
+        fullTitle: 'ISPLATA VISA',
+        location: null,
+        mcc: null
+      },
+      movements: [
+        {
+          account: {
+            id: '1234567895'
+          },
+          fee: 0,
+          id: null,
+          invoice: null,
+          sum: -378.85
+        }
+      ]
+    }, {
       comment: null,
       date: new Date('2023-12-06T00:00:00'),
       hold: false,
