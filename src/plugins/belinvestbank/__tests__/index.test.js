80591bc34375f0074747d2a8512e87a766cda31b
Svyatoslav Kulich <47153414+Kabaye@users.noreply.github.com>
2024-05-28 12:51:00 +0300

    [belinvestbank] update version to actual (#748)

--- src/plugins/belinvestbank/__tests__/index.test.js
+++ src/plugins/belinvestbank/__tests__/index.test.js
@@ -389,7 +389,7 @@ function mockApiLoginAndPass () {
       login: '123456789',
       password: 'pass',
       deviceId: 'device id',
-      versionApp: '2.15.2',
+      versionApp: '2.19.0',
       os: 'Android',
       device_token: 'device token',
       device_token_type: 'ANDROID'
