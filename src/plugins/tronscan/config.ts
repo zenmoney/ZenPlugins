3c2c8c1b387048f2e9bd4225b7cb55c920a457d9
Aleksandr Petrov <dzhiriki@gmail.com>
2024-08-06 19:33:58 +0300

    [tronscan] add TRX support

--- src/plugins/tronscan/config.ts
+++ src/plugins/tronscan/config.ts
@@ -1,1 +1,36 @@
-export const SUPPORTED_TOKENS = ['USDT', 'TUSD', 'USDD', 'USDC']
+export const TOKENS_CONFIG: Record<string, { currency: string, title?: string }> = {
+  trx: {
+    currency: 'TRX',
+    title: 'TRON'
+  },
+  USDT: {
+    currency: 'USDT'
+  },
+  TUSD: {
+    currency: 'USDT'
+  },
+  USDD: {
+    currency: 'USDT'
+  },
+  USDC: {
+    currency: 'USDT'
+  }
+}
+
+export const SUPPORTED_TOKENS = Object.keys(TOKENS_CONFIG)
+export type SupportedToken = (typeof SUPPORTED_TOKENS)[number]
+export interface TokenInfo {
+  tokenId: string
+  tokenName: string
+  tokenAbbr: string
+  tokenDecimal: number
+  quantity?: number | string
+}
+
+export interface SupportedTokenInfo extends TokenInfo {
+  tokenAbbr: SupportedToken
+}
+
+export function isSupportedToken (tokenInfo: TokenInfo): tokenInfo is SupportedTokenInfo {
+  return (SUPPORTED_TOKENS).includes(tokenInfo.tokenAbbr)
+}
--- src/plugins/tronscan/index.ts
+++ src/plugins/tronscan/index.ts
@@ -1,30 +1,68 @@
 import { Account, ScrapeFunc, Transaction } from '../../types/zenmoney'
 import { Preferences, tronscanApi } from './api'
-import { convertAccount, convertTransaction } from './converters'
+import { isSupportedToken } from './config'
+import { convertAccount, convertTransaction, convertTokenTransaction } from './converters'
 
-export const scrape: ScrapeFunc<Preferences> = async ({ preferences, fromDate, toDate }) => {
+export const scrape: ScrapeFunc<Preferences> = async ({
+  preferences,
+  fromDate,
+  toDate
+}) => {
   const accounts: Account[] = []
   const transactions: Transaction[] = []
 
   await Promise.all(
     preferences.wallets
       .split(',')
-      .map(wallet => wallet.trim())
+      .map((wallet) => wallet.trim())
       .map(async (wallet) => {
-        const [tokens, transfers] = await Promise.all([
+        const [tokens, walletTransactions, trxTransfers, transfers] = await Promise.all([
           tronscanApi.fetchTokens(wallet),
-          tronscanApi.fetchTransfers(wallet, fromDate, toDate)
+          tronscanApi.fetchTransactions(wallet, fromDate, toDate),
+          tronscanApi.fetchTronTransfers(wallet, fromDate, toDate),
+          tronscanApi.fetchTokenTransfers(wallet, fromDate, toDate)
         ])
 
-        const walletAccounts: Account[] = tokens.map(t => convertAccount(t, wallet))
+        const walletAccounts: Account[] = tokens.map((t) =>
+          convertAccount(t, wallet)
+        )
 
-        const walletTransactions = transfers
-          .map((transaction) => convertTransaction(transaction, wallet, tokens))
-          .filter((transaction): transaction is Transaction => transaction !== null)
+        const trxTransactions = trxTransfers
+          .map((transfer) => {
+            const transaction = walletTransactions.get(transfer.transaction_id)
+            return convertTransaction(transfer, wallet, transaction)
+          })
+          .filter(
+            (transaction): transaction is Transaction => transaction !== null
+          )
+        const tokenTransactions = transfers
+          .flatMap((transfer) => {
+            const transaction = walletTransactions.get(transfer.transaction_id)
+            return convertTokenTransaction(transfer, wallet, transaction)
+          })
+          .filter(
+            (transaction): transaction is Transaction => transaction !== null
+          )
 
         accounts.push(...walletAccounts)
-        transactions.push(...walletTransactions)
-      }))
+        transactions.push(...trxTransactions)
+        transactions.push(...tokenTransactions)
+
+        // fetchTokens doesn't return tokens with zero balance
+        // so we need to collect them from transactions
+        const existedAccounts = new Set(tokens.map((t) => t.tokenId))
+        for (const transfer of [...trxTransfers, ...transfers]) {
+          const tokenInfo = transfer.tokenInfo
+
+          if (existedAccounts.has(tokenInfo.tokenId) || !isSupportedToken(tokenInfo)) {
+            continue
+          }
+
+          accounts.push(convertAccount(tokenInfo, wallet))
+          existedAccounts.add(tokenInfo.tokenId)
+        }
+      })
+  )
 
   return {
     accounts,
