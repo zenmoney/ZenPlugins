8472ffc7d39d0ef0c2bd84fd91d856b70ccc1072
Viktor Oreshkin <imselfish@stek29.rocks>
2024-10-18 05:22:10 +0300

    etherscan: change instrument for stablecoins to USDT
    change USDT to match actual instrument
    change USDC/BUSD to match Troncoin

--- src/plugins/etherscan/tokens/config.ts
+++ src/plugins/etherscan/tokens/config.ts
@@ -13,19 +13,19 @@ export const SUPPORTED_TOKENS: TokenConfig[] = [
   {
     title: 'USDT',
     contractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-    instrument: 'USD',
+    instrument: 'USDT',
     convertBalance: (value) => value / 1000000
   },
   {
     title: 'USDC',
     contractAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    instrument: 'USD',
+    instrument: 'USDT',
     convertBalance: (value) => value / 1000000
   },
   {
     title: 'BUSD',
     contractAddress: '0x4fabb145d64652a948d72533023f6e7a623c7c53',
-    instrument: 'USD',
+    instrument: 'USDT',
     convertBalance: (value) => value / 1000000
   }
 ]
