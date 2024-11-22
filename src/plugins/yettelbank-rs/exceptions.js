5daa698855b29ae9149f8d894d696763c61c2143
yellalena <yellalena@gmail.com>
2024-11-22 09:17:56 +0100

    deleted generated js files

--- src/plugins/yettelbank-rs/exceptions.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errors_1 = require("../../errors");
--- src/plugins/yettelbank-rs/exceptions.ts
+++
@@ -1,5 +0,0 @@
-import { ZPAPIError } from "../../errors";
-
-export declare class AccountsInfoNotFound extends ZPAPIError {
-    constructor (message?: string)
-  }
