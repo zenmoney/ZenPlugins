5b9a4926416fe70b2b454d882c05dad0d6fde056
Aleksandr Beliakov <belaleksigor@gmail.com>
2024-12-19 11:41:13 +0100

    change some unit tests values

--- src/plugins/otp-rs/__tests__/accounts.ts
+++ src/plugins/otp-rs/__tests__/accounts.ts
@@ -5,7 +5,7 @@ describe('parseAccounts', () => {
     const apiAccounts: string[][] = [
       [
         '',
-        '325930070731111111',
+        '1234567890',
         'Tekući računi fizičkih lica u RSD',
         'RSD',
         '555555.75',
@@ -22,7 +22,7 @@ describe('parseAccounts', () => {
         '',
         'N',
         'True',
-        '325930070731111111',
+        '1234567890',
         '3610.00',
         '',
         '1',
@@ -30,11 +30,11 @@ describe('parseAccounts', () => {
       ],
       [
         '',
-        '325930070731222222',
+        '0987654321',
         'Tekući računi fizičkih lica u EUR',
         'EUR',
-        '0.00',
-        '0.00',
+        '100.00',
+        '100.00',
         '',
         '',
         '',
@@ -47,7 +47,7 @@ describe('parseAccounts', () => {
         '',
         'N',
         'True',
-        '325930070731222222',
+        '0987654321',
         '0.00',
         '',
         '1',
@@ -60,17 +60,17 @@ describe('parseAccounts', () => {
     expect(result).toHaveLength(2)
 
     // Check RSD account
-    expect(result[0].accountNumber).toBe('325930070731111111')
+    expect(result[0].accountNumber).toBe('1234567890')
     expect(result[0].description).toBe('Tekući računi fizičkih lica u RSD')
     expect(result[0].currencyCode).toBe('RSD')
     expect(result[0].balance).toBe(545555.75)
     expect(result[0].currencyCodeNumeric).toBe('941')
 
     // Check EUR account
-    expect(result[1].accountNumber).toBe('325930070731222222')
+    expect(result[1].accountNumber).toBe('0987654321')
     expect(result[1].description).toBe('Tekući računi fizičkih lica u EUR')
     expect(result[1].currencyCode).toBe('EUR')
-    expect(result[1].balance).toBe(0)
+    expect(result[1].balance).toBe(100)
     expect(result[1].currencyCodeNumeric).toBe('978')
   })
 })
