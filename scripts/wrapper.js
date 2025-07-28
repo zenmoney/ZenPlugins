72770a043d1b6af631edfb01e368ed6fbee49e4a
Konstantin Sychkin <konstantin.sychkin@gmail.com>
2025-07-28 14:01:15 +0300

    fix wrapper script

--- scripts/wrapper.js
+++ scripts/wrapper.js
@@ -1,8 +1,16 @@
-const { execSync } = require('child_process')
+const { spawnSync } = require('child_process')
 const os = require('os')
 
-const argv = process.argv
-execSync(argv[2] + argv.slice(3).join(','), {
+const command = process.argv[2]
+let args = process.argv.slice(3)
+const pluginIndex = args.indexOf('PLUGIN=')
+if (pluginIndex !== -1) {
+  const pluginValue = args.slice(pluginIndex + 1).join(',')
+  args = args.slice(0, pluginIndex).concat(`PLUGIN=${pluginValue}`)
+}
+
+const result = spawnSync(command, args, {
   env: { ...process.env, NODE_OPTIONS: `--max-old-space-size=${Math.floor(os.totalmem() / (1024 * 1024))}` },
   stdio: 'inherit'
 })
+process.exit(result.status)
