b048d2aa1361e24fc98f5c9aabb20ea6fac20455
Uladzislau Zhalankouski <krislintigo@gmail.com>
2026-05-07 16:47:42 +0300

    [mtbank] add anti-bot headers and cookie (#1004)

--- src/plugins/mtbank/index.js
+++ src/plugins/mtbank/index.js
@@ -2,14 +2,18 @@ import { fetchAccounts, fetchTransactions, login } from './api'
 import { convertAccount, convertTransaction, filterDuplicates } from './converters'
 
 export async function scrape ({ preferences, fromDate, toDate }) {
-  const token = await login(preferences.phone, preferences.password)
-  const accounts = (await fetchAccounts(token))
+  const cookies = await login(preferences.phone, preferences.password)
+
+  const accounts = (await fetchAccounts(cookies))
     .map(convertAccount)
     .filter(account => account !== null)
-  let transactions = (await fetchTransactions(token, accounts, fromDate, toDate))
+
+  let transactions = (await fetchTransactions(cookies, accounts, fromDate, toDate))
     .map(transaction => convertTransaction(transaction, accounts))
     .filter(transaction => transaction !== null)
+
   transactions = filterDuplicates(transactions)
+
   return {
     accounts,
     transactions
