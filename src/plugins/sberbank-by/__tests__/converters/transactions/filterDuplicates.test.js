95ac47f94c3fe65b01368e74187639826f516a59
Svyatoslav Kulich <47153414+Kabaye@users.noreply.github.com>
2026-06-08 15:22:48 +0300

    [sberbank-by] prefer posted transactions over holds, tidy deduplication helpers (#1037)

--- src/plugins/sberbank-by/__tests__/converters/transactions/filterDuplicates.test.js
+++ src/plugins/sberbank-by/__tests__/converters/transactions/filterDuplicates.test.js
@@ -109,6 +109,299 @@ describe('filterDuplicates', () => {
           comment: 'Открытие'
         }
       ]
+    ],
+    [
+      [
+        {
+          sourceSystem: 4,
+          eventId: 'hold-event',
+          contractId: '10957794',
+          cardPAN: '911238******4812',
+          cardId: '1771310095',
+          eventDate: 1712911020000,
+          transactionCode: '01000R',
+          transactionName: 'SOU Sber Bank > Minsk BLR Терминал: WWL90902 RRN:410383389811 AuthCode:602242',
+          merchantId: '0802082',
+          merchantPlace: 'SOU Sber Bank > Minsk BLR',
+          transactionSum: -50,
+          transactionCurrency: '933',
+          commissionSum: 0,
+          commissionCurrency: '933',
+          rnnCode: '410383389811',
+          authorizationCode: '602242',
+          eventStatus: 0,
+          payAvailable: false
+        },
+        {
+          sourceSystem: 1,
+          eventId: 'posted-event',
+          contractId: '10957794',
+          contractCurrency: '933',
+          cardPAN: '911238******4812',
+          cardId: '1771310095',
+          eventDate: 1712911022000,
+          processingDate: 1712914622000,
+          transactionType: -1,
+          transactionCode: '205',
+          transactionName: 'Покупка SOU Sber Bank',
+          operationCode: null,
+          merchantId: '0802082',
+          merchantName: 'SOU Sber Bank',
+          merchantPlace: 'Minsk',
+          transactionSum: 50,
+          transactionCurrency: '933',
+          accountSum: 50,
+          commissionSum: 0,
+          commissionCurrency: '933',
+          rnnCode: '410383389811',
+          authorizationCode: '602242',
+          eventStatus: 1,
+          payAvailable: false
+        }
+      ],
+      {
+        10957794: {
+          id: 'account-1',
+          instrument: 'BYN'
+        }
+      },
+      [
+        {
+          hold: false,
+          date: new Date('2024-04-12T08:37:02.000Z'),
+          movements: [
+            {
+              id: 'posted-event',
+              account: { id: 'account-1' },
+              invoice: null,
+              sum: -50,
+              fee: 0
+            }
+          ],
+          merchant: null,
+          comment: 'Покупка SOU Sber Bank'
+        }
+      ]
+    ],
+    [
+      [
+        {
+          sourceSystem: 4,
+          eventId: 'hold-event',
+          contractId: '10957794',
+          cardPAN: '911238******4812',
+          cardId: '1771310095',
+          eventDate: 1712911020000,
+          transactionCode: '01000R',
+          transactionName: 'SOU Sber Bank > Minsk BLR Терминал: WWL90902 RRN:410383389811 AuthCode:602242',
+          merchantId: '0802082',
+          merchantPlace: 'SOU Sber Bank > Minsk BLR',
+          transactionSum: -50,
+          transactionCurrency: '933',
+          commissionSum: 0,
+          commissionCurrency: '933',
+          rnnCode: '410383389811',
+          authorizationCode: '602242',
+          eventStatus: 0,
+          payAvailable: false
+        },
+        {
+          sourceSystem: 1,
+          eventId: 'posted-event',
+          contractId: '10957794',
+          contractCurrency: '933',
+          cardPAN: '911238******4812',
+          cardId: '1771310095',
+          eventDate: 1712911022000,
+          processingDate: 1712914622000,
+          transactionType: -1,
+          transactionCode: '205',
+          transactionName: 'Покупка SOU Sber Bank',
+          operationCode: null,
+          merchantId: '0802082',
+          merchantName: 'SOU Sber Bank',
+          merchantPlace: 'Minsk',
+          transactionSum: 50,
+          transactionCurrency: '933',
+          accountSum: 50,
+          commissionSum: 0,
+          commissionCurrency: '933',
+          rnnCode: '410383389811',
+          authorizationCode: '000000',
+          eventStatus: 1,
+          payAvailable: false
+        }
+      ],
+      {
+        10957794: {
+          id: 'account-1',
+          instrument: 'BYN'
+        }
+      },
+      [
+        {
+          hold: false,
+          date: new Date('2024-04-12T08:37:02.000Z'),
+          movements: [
+            {
+              id: 'posted-event',
+              account: { id: 'account-1' },
+              invoice: null,
+              sum: -50,
+              fee: 0
+            }
+          ],
+          merchant: null,
+          comment: 'Покупка SOU Sber Bank'
+        }
+      ]
+    ],
+    [
+      [
+        {
+          sourceSystem: 4,
+          eventId: 'hold-event',
+          contractId: '10957794',
+          cardPAN: '911238******4812',
+          cardId: '1771310095',
+          eventDate: 1712911020000,
+          transactionCode: '01000R',
+          transactionName: 'SOU Sber Bank > Minsk BLR Терминал: WWL90902 RRN:410383389811 AuthCode:602242',
+          merchantId: '0802082',
+          merchantPlace: 'SOU Sber Bank > Minsk BLR',
+          transactionSum: -50,
+          transactionCurrency: '933',
+          commissionSum: 0,
+          commissionCurrency: '933',
+          rnnCode: '410383389811',
+          authorizationCode: '602242',
+          eventStatus: 0,
+          payAvailable: false
+        },
+        {
+          sourceSystem: 1,
+          eventId: 'posted-event',
+          contractId: '10957794',
+          contractCurrency: '933',
+          cardPAN: '911238******4812',
+          cardId: '',
+          eventDate: 1712911022000,
+          processingDate: 1712914622000,
+          transactionType: -1,
+          transactionCode: '205',
+          transactionName: 'Покупка SOU Sber Bank',
+          operationCode: null,
+          merchantId: '0802082',
+          merchantName: 'SOU Sber Bank',
+          merchantPlace: 'Minsk',
+          transactionSum: 50,
+          transactionCurrency: '933',
+          accountSum: 50,
+          commissionSum: 0,
+          commissionCurrency: '933',
+          rnnCode: '410383389811',
+          authorizationCode: '602242',
+          eventStatus: 1,
+          payAvailable: false
+        }
+      ],
+      {
+        10957794: {
+          id: 'account-1',
+          instrument: 'BYN'
+        }
+      },
+      [
+        {
+          hold: false,
+          date: new Date('2024-04-12T08:37:02.000Z'),
+          movements: [
+            {
+              id: 'posted-event',
+              account: { id: 'account-1' },
+              invoice: null,
+              sum: -50,
+              fee: 0
+            }
+          ],
+          merchant: null,
+          comment: 'Покупка SOU Sber Bank'
+        }
+      ]
+    ],
+    [
+      [
+        {
+          sourceSystem: 8,
+          eventId: '-696460788586#7#25',
+          contractId: '20301567',
+          contractCurrency: '933',
+          cardPAN: '',
+          eventDate: 1779901343000,
+          processingDate: 1779829200000,
+          transactionType: 1,
+          transactionCode: 'TRAN_ERIP',
+          transactionName: 'Пополнение счета через ЕРИП онлайн',
+          transactionSum: 20,
+          transactionCurrency: '933',
+          eventStatus: 1,
+          payAvailable: false
+        },
+        {
+          sourceSystem: 8,
+          eventId: '-696460788586#7#26',
+          contractId: '20301567',
+          contractCurrency: '933',
+          cardPAN: '',
+          eventDate: 1779901343000,
+          processingDate: 1779829200000,
+          transactionType: 1,
+          transactionCode: 'TRAN_ERIP',
+          transactionName: 'Пополнение счета через ЕРИП онлайн',
+          transactionSum: 20,
+          transactionCurrency: '933',
+          eventStatus: 1,
+          payAvailable: false
+        }
+      ],
+      {
+        20301567: {
+          id: 'account-1',
+          instrument: 'BYN'
+        }
+      },
+      [
+        {
+          hold: false,
+          date: new Date('2026-05-27T17:02:23.000Z'),
+          movements: [
+            {
+              id: '-696460788586#7#25',
+              account: { id: 'account-1' },
+              invoice: null,
+              sum: 20,
+              fee: 0
+            }
+          ],
+          merchant: null,
+          comment: 'Пополнение счета через ЕРИП онлайн'
+        },
+        {
+          hold: false,
+          date: new Date('2026-05-27T17:02:23.000Z'),
+          movements: [
+            {
+              id: '-696460788586#7#26',
+              account: { id: 'account-1' },
+              invoice: null,
+              sum: 20,
+              fee: 0
+            }
+          ],
+          merchant: null,
+          comment: 'Пополнение счета через ЕРИП онлайн'
+        }
+      ]
     ]
   ])('converts filter duplicates transactions', (apiTransactions, accountsByContractNumber, transactions) => {
     expect(convertTransactions(apiTransactions, accountsByContractNumber)).toEqual(transactions)
