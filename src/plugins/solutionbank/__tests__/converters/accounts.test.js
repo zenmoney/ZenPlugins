658be5f4985a77ef9bc59b9db19d8bac6a450911
Svyatoslav Kulich <47153414+Kabaye@users.noreply.github.com>
2026-06-08 15:42:24 +0300

    [solutionbank] update api (#1062)

--- src/plugins/solutionbank/__tests__/converters/accounts.test.js
+++ src/plugins/solutionbank/__tests__/converters/accounts.test.js
@@ -3,7 +3,7 @@ import { convertAccount } from '../../converters'
 describe('convertAccount', () => {
   it('should convert checking account', () => {
     const account = convertAccount({
-      accountNumber: 'BY11RSHN10111010100110000000',
+      accountNumber: 'mock-iban-001',
       accountStatus: 'OPEN',
       accountType: '1',
       agreementDate: 1589811000000,
@@ -16,13 +16,13 @@ describe('convertAccount', () => {
       canRefillOtherCurrency: false,
       canRefillSameCurrency: false,
       canSell: false,
-      cardAccountNumber: '00750933012345',
+      cardAccountNumber: 'mock-account-001',
       cards: [
         {
           canChange3D: true,
           canChangeSmsNotification: true,
-          cardDepartmentAddress: 'г. Минск;ул. Сторожевская 8',
-          cardHash: 'h5jaI5li6snrBC2lkrLIEnCzS§l0aNIj5PaRLx6C0OGaBofDbE6_v1KfVbK4iNxT1W2RkcRrrEcLVmhDGd3jew',
+          cardDepartmentAddress: 'г. Тестовый город;ул. Тестовая 1',
+          cardHash: 'mock-card-hash-001',
           cardNumberMasked: '4*** **** **** 1111',
           cardStatus: 'OPEN',
           cardType: {
@@ -45,28 +45,31 @@ describe('convertAccount', () => {
       ],
       contractId: '88990011',
       currency: '933',
-      ibanNum: 'BY11RSHN10111010100110000000',
+      ibanNum: 'mock-iban-001',
       interestRate: 1e-06,
-      internalAccountId: '00750933012345',
+      internalAccountId: 'mock-account-001',
       openDate: 1589811000000,
       percentsLeft: '0.0000010',
       productCode: '300750',
       productName: 'Visa Virtual',
       rkcCode: '701',
-      rkcName: 'ЦБУ 07/01 г.Минск, ЗАО "Банк "Решение"',
+      rkcName: 'Тестовое отделение',
       balance: '10,1'
     })
 
     expect(account).toEqual({
-      id: '00750933012345',
+      id: 'mock-account-001-88990011',
       title: 'Visa Virtual*1111',
       type: 'card',
       instrument: 'BYN',
       instrumentCode: '933',
       balance: 10.1,
-      syncID: ['1111'],
+      syncID: ['88990011', '1111'],
       productType: 'Visa Virtual',
-      cardHash: 'h5jaI5li6snrBC2lkrLIEnCzS§l0aNIj5PaRLx6C0OGaBofDbE6_v1KfVbK4iNxT1W2RkcRrrEcLVmhDGd3jew',
+      cardHash: 'mock-card-hash-001',
+      cardLast4: '1111',
+      internalAccountId: 'mock-account-001',
+      cardAccountNumber: 'mock-account-001',
       bankCode: '288',
       accountType: '1',
       rkcCode: '701'
