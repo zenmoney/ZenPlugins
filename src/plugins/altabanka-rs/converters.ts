56868a29af535d15695f38c8874787b3a247c4bd
Katrin Torsunova <etorsunova@marfa-tech.com>
2026-06-30 17:32:38 +0200

    update converters.ts

--- src/plugins/altabanka-rs/converters.ts
+++ src/plugins/altabanka-rs/converters.ts
@@ -1,116 +1,71 @@
 import { AccountOrCard, AccountType, ExtendedTransaction } from '../../types/zenmoney'
 import { AccountInfo, AccountTransaction } from './types'
-import { toISODateString, dateInTimezone } from '../../common/dateUtils'
 
 export function convertAccounts (apiAccounts: AccountInfo[]): AccountOrCard[] {
-  return apiAccounts.map(apiAccount => {
-    return {
-      id: apiAccount.id,
-      type: AccountType.ccard,
-      title: apiAccount.name,
-      instrument: apiAccount.currency,
-      balance: apiAccount.balance,
-      available: apiAccount.balance,
-      creditLimit: 0,
-      syncIds: [apiAccount.id]
-    }
-  })
+  return apiAccounts.map(account => ({
+    id: account.iban,
+    type: AccountType.checking,
+    title: account.name,
+    instrument: account.currency,
+    balance: account.balance,
+    syncIds: [account.iban, account.accountNumber]
+  }))
 }
 
-export function convertTransaction (accountTransaction: AccountTransaction, account: AccountInfo, hold = false): ExtendedTransaction | null {
-  accountTransaction.currency = accountTransaction.currency === undefined
-    ? 'RSD'
-    : accountTransaction.currency
-
-  if (accountTransaction.amount === 0) {
+export function convertTransaction (apiTransaction: AccountTransaction, account: AccountInfo): ExtendedTransaction | null {
+  if (apiTransaction.amount === 0) {
     return null
-  }
-  let invoice = {
-    sum: accountTransaction.amount,
-    instrument: accountTransaction.currency
-  }
-  const match = accountTransaction.description.match(/^([\d.]+)\s(\w{3})/)
-  if (match != null) {
-    invoice = {
-      sum: accountTransaction.amount < 0 ? -parseFloat(match[1]) : parseFloat(match[1]),
-      instrument: match[2]
-    }
   }
 
   const transaction: ExtendedTransaction = {
-    hold,
-    date: accountTransaction.date,
+    hold: false,
+    date: apiTransaction.date,
     movements: [
       {
-        id: accountTransaction.id,
-        account: { id: account.id },
-        sum: invoice?.instrument !== account.currency ? null : accountTransaction.amount,
+        id: apiTransaction.id,
+        account: { id: account.iban },
+        sum: apiTransaction.currency === account.currency ? apiTransaction.amount : null,
         fee: 0,
-        invoice: invoice?.instrument === account.currency ? null : invoice
+        invoice: apiTransaction.currency !== account.currency && apiTransaction.currency !== undefined
+          ? { sum: apiTransaction.amount, instrument: apiTransaction.currency }
+          : null
       }
     ],
     merchant: null,
-    comment: accountTransaction.description !== '' && (match == null) ? accountTransaction.description : null
-  };
-  [
-    parseCashTransfer,
-    parseInnerTransfer,
-    parsePayeeAndComment
-  ].some(parser => parser(transaction, accountTransaction, account, invoice))
-  return transaction
-}
+    comment: null
+  }
 
-function parseCashTransfer (transaction: ExtendedTransaction, accountTransaction: AccountTransaction, account: AccountInfo, invoice: { sum: number, instrument: string }): boolean {
-  if ([
-    /ATM/,
-    /Gotovinska/i
-  ].some(regexp => regexp.test(accountTransaction.address))) {
+  if (isCash(apiTransaction.description)) {
     transaction.movements.push({
       id: null,
       account: {
         company: null,
-        instrument: invoice.instrument,
+        instrument: apiTransaction.currency ?? account.currency,
         syncIds: null,
         type: AccountType.cash
       },
       invoice: null,
-      sum: -invoice.sum,
+      sum: -apiTransaction.amount,
       fee: 0
     })
+    transaction.comment = apiTransaction.description
+  } else if (isTransfer(apiTransaction.description)) {
+    transaction.comment = apiTransaction.description
+  } else {
+    transaction.merchant = {
+      fullTitle: apiTransaction.description,
+      mcc: null,
+      location: null
+    }
   }
-  return false
+
+  return transaction
 }
 
-function parseInnerTransfer (transaction: ExtendedTransaction, accountTransaction: AccountTransaction, account: AccountInfo, invoice: { sum: number, instrument: string }): boolean {
-  if ([
-    /Interni transfer/i,
-    /Kupovina deviza/i
-  ].some(regex => regex.test(accountTransaction.address))) {
-    const idCurrency = accountTransaction.id.split('%')[0]
-    transaction.groupKeys = [
-      `${toISODateString(dateInTimezone(transaction.date, 180)).slice(0, 10)}_${invoice.instrument}_${Math.abs(invoice.sum)}`,
-      `${idCurrency}`
-    ]
-    transaction.merchant = null
-    transaction.comment = accountTransaction.address
-
-    return true
-  }
-  return false
+function isCash (description: string): boolean {
+  return /ATM|Gotovinska/i.test(description)
 }
 
-function parsePayeeAndComment (transaction: ExtendedTransaction, accountTransaction: AccountTransaction, account: AccountInfo, invoice: { sum: number, instrument: string }): boolean {
-  transaction.merchant = {
-    fullTitle: accountTransaction.address,
-    mcc: null,
-    location: null
-  }
-  if ([
-    /Uplata/i,
-    /Gotovinska/i
-  ].some(regex => regex.test(accountTransaction.address))) {
-    transaction.merchant = null
-    transaction.comment = accountTransaction.address
-  }
-  return false
+function isTransfer (description: string): boolean {
+  return /Interni transfer|Kupovina deviza|Uplata/i.test(description)
 }
